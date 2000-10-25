4e487c5c7b84a1eadf63cb3691c2471005132410
Madhava Syamlal <madhava.syamlal@netl.doe.gov>
2000-10-25 19:50:41 +0000

    Removed files common to model directory

--- post_mfix/exit.f
+++
@@ -1,6 +0,0 @@
-      subroutine mfix_exit 
-!...Translated by Pacific-Sierra Research VAST-90 2.06G5  12:17:31  12/09/98  
-!...Switches: -xf
-      implicit none
-      stop  
-      end subroutine mfix_exit 
