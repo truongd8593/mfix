b81e2fe6d91ede303ef543cc2ea6eb253f2bd672
Madhava Syamlal <madhava.syamlal@netl.doe.gov>
2004-10-01 15:28:02 +0000

    XMIN problem fix.

--- post_mfix/calc_cell2.f
+++ post_mfix/calc_cell2.f
@@ -1,6 +1,6 @@
 !vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
 !                                                                      C
-!  Module name: CALC_CELL2 (REACTOR_LOC,D_DIR,N_DIR,CELL_LOC)          C
+!  Module name: CALC_CELL2 (RMIN, REACTOR_LOC,D_DIR,N_DIR,CELL_LOC)    C
 !  Purpose: calculate the cell index for a reactor location            C
 !                                                                      C
 !  Author: P. Nicoletti                               Date: 02-DEC-91  C
@@ -20,13 +20,15 @@
 !                                                                      C
 !^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
 !
-      SUBROUTINE CALC_CELL2 (REACTOR_LOC,D_DIR,N_DIR,CELL_LOC)
+      SUBROUTINE CALC_CELL2 (RMIN, REACTOR_LOC,D_DIR,N_DIR,CELL_LOC)
 !
 	Use param
 	Use param1
 !
 ! passed arguments
 !
+!    RMIN       - starting value of the axis -- XMIN need not be zero,
+!                   YMIN and ZMIN are assumed to be zero
 !    REACTOR_LOC - location along one of the axis for which the cell
 !                  index is to be found
 !    D_DIR       - cell lengths (DX,DY,DZ)
@@ -40,10 +42,10 @@
 !
       IMPLICIT NONE
       INTEGER           N_DIR , CELL_LOC , LC
-      DOUBLE PRECISION  REACTOR_LOC , D_DIR(*) , CELL_START , CELL_END
+      DOUBLE PRECISION  REACTOR_LOC , D_DIR(*) , CELL_START , CELL_END, RMIN
 !
       CELL_LOC = - 1
-      CELL_START = 0.0
+      CELL_START = RMIN
       DO 100 LC = 2,N_DIR+1
          CELL_END   = CELL_START + D_DIR(LC)
          IF (REACTOR_LOC .GE.CELL_START .AND. &
