1509bda6512e57a6b726ac72b458a74ffd1f825d
Sofiane Benyahia <sofiane.benyahia@netl.doe.gov>
2005-05-05 16:29:54 +0000

    replacing MAX_SOLID_1_PACKING with ep_s_max(1) etc. because of changes in calc_p_star

--- model/calc_p_star.f
+++ model/calc_p_star.f
@@ -96,7 +96,7 @@
 
 !TO DO:
 ! Extend the formulation for more than two sizes
-!
+! sof (done)
 !vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
 !                                                                      C
 !  Module name: CALC_ep_star(IJK,IER)                                  C
@@ -104,9 +104,14 @@
 !                                                                      C
 !  Author: D. Gera/M. Syamlal                         Date: 31-DEC-02  C
 !  Reviewer:                                          Date:            C
+!  Modified: Sof                                      Date: 02-May-05  C
 !                                                                      C
 !                                                                      C
 !  Literature/Document References:                                     C
+!                                                                      C
+!    A.B. Yu and N. Standish. Powder Tech, 52 (1987) 233-241           C
+! The commented version implemented by Dinesh has the following ref:   C
+!     R.F. Fedors and R.F. Landel. Powder Tech, 23 (1979) 225-231      C
 !                                                                      C
 !  Variables referenced:                                               C
 !  Variables modified:                                                 C
@@ -129,6 +134,7 @@
       USE indices
       USE physprop
       USE constant
+      USE toleranc
       USE compar
       IMPLICIT NONE
 !-----------------------------------------------
@@ -142,8 +148,28 @@
       INTEGER          IER
 !
 !                      Indices
-      INTEGER          IJK
+      INTEGER          IJK, I, J
       double precision xbar
+!
+! start sof modifications (02-May-05)
+!
+!                       maximum packing for the mixture
+       DOUBLE PRECISION P_IT(MMAX)
+!
+!                       maximum packing fraction for a binary mixture
+       DOUBLE PRECISION P_IJ(MMAX, MMAX)
+!
+!                       particle diameter ratio
+       DOUBLE PRECISION R_IJ(MMAX, MMAX)
+!      
+!                       fractional solids volume corresponding to P_IJ
+       DOUBLE PRECISION X_IJ(MMAX, MMAX)
+!      
+!                       fractional solids volume in a mixture
+       DOUBLE PRECISION COMP_X_I(MMAX), SUM_LOCAL ! this is Xj in eq. 22 of Yu-Standish
+!      
+!                       tmp variables for rearanging solids phases from coarsest to finest
+       DOUBLE PRECISION DP_TMP(MMAX), EPs_TMP(MMAX), EPs_max_TMP(MMAX), old_value
 !-----------------------------------------------
       INCLUDE 'ep_s1.inc'
       INCLUDE 'fun_avg1.inc'
@@ -151,22 +177,150 @@
       INCLUDE 'fun_avg2.inc'
       INCLUDE 'ep_s2.inc'
 !
- 
- 	if ((EP_s(IJK,1)+EP_s(IJK,2)) .NE. 0) THEN
-	   xbar = EP_s(IJK,1)/(EP_s(IJK,1)+EP_s(IJK,2))
+! rearanging phases to start from coarsest to finest particles
+! this is the way the algorithm was written by Yu and Standish.
+!
+       DO I = 1, MMAX
+         
+	 DP_TMP(I) = D_P(I)
+	 EPs_TMP(I) = EP_s(IJK,I)
+	 EPs_max_TMP(I) = ep_s_max(I)
+       END DO
 
-	   if (xbar .LE. ep_s_max_ratio(1,2)) THEN
-	      CALC_EP_star =MAX(0.36d0, (1.-(((ep_s_max(1)-ep_s_max(2))+&
-              (1.-d_p_ratio(1,2))*(1-ep_s_max(1))*ep_s_max(2))*(ep_s_max(1)+&
-              (1-ep_s_max(1)) *ep_s_max(2))*xbar/ep_s_max(1)+ep_s_max(2))))
-     	   else
-    	      CALC_EP_star =MAX(0.36d0, (1.-((1. -d_p_ratio(1,2))*(ep_s_max(1)&
-              +(1-ep_s_max(1))*ep_s_max(2))*(1. -xbar) +ep_s_max(1))))
-	   end if
-	else
-	   CALC_EP_star = MIN(ep_s_max(1), ep_s_max(2))
-	end if
- 
+       DO I = 1, MMAX	 
+	 DO J = I, MMAX
+	   
+	   IF(DP_TMP(I) < DP_TMP(J)) THEN
+	     
+	     old_value = DP_TMP(I)
+	     DP_TMP(I) = DP_TMP(J)
+	     DP_TMP(J) = old_value
+	     
+	     old_value = EPs_TMP(I)
+	     EPs_TMP(I) = EPs_TMP(J)
+	     EPs_TMP(J) = old_value
+	     
+	     old_value = EPs_max_TMP(I)
+	     EPs_max_TMP(I) = EPs_max_TMP(J)
+	     EPs_max_TMP(J) = old_value
+
+	   ENDIF
+	   
+	 ENDDO
+       END DO
+!
+!end of solids phase rearangment
+!
+! compute equations 25 in Yu-Standish
+!      
+       DO I = 1, MMAX
+         SUM_LOCAL = ZERO
+	 DO J = 1, MMAX
+
+	   IF( I .GE. J) THEN
+	     R_IJ(I, J) = DP_TMP(I)/DP_TMP(J)
+	   ELSE
+	     R_IJ(I, J) = DP_TMP(J)/DP_TMP(I)
+	   ENDIF
+	 SUM_LOCAL = SUM_LOCAL + EPs_TMP(J)
+	 END DO
+	 
+	 IF(SUM_LOCAL > DIL_EP_s) THEN
+	   COMP_X_I(I) = EPs_TMP(I)/SUM_LOCAL ! fractional solids volume see eq. 20
+	 ELSE
+	   CALC_EP_star = EPs_max_TMP(1) !return first phase ep_s_max in case very dilute
+	   RETURN
+	 ENDIF
+       
+       END DO     
+!
+! compute equation 23-24 in Yu-Standish
+!
+       DO I = 1, MMAX
+         DO J = 1, MMAX
+	   
+	   IF( R_IJ(I, J) .LE. 0.741) THEN
+
+	     IF( J .LT. I ) THEN
+	       X_IJ(I, J) = (1.0 - R_IJ(I, J)*R_IJ(I, J))/(2.0 -  EPs_max_TMP(I))
+	     ELSE
+	       X_IJ(I, J) = 1.0 - (1.0 - R_IJ(I, J)*R_IJ(I, J))/(2.0 -  EPs_max_TMP(I))
+	     ENDIF
+
+	     P_IJ(I, J) = EPs_max_TMP(I) + EPs_max_TMP(I)* (1.0-EPs_max_TMP(I)) *  &
+	                  (1.0 - 2.35*R_IJ(I, J) + 1.35*R_IJ(I, J)*R_IJ(I, J))
+	   ELSE
+	   
+	     P_IJ(I, J) = EPs_max_TMP(I)
+	   ENDIF
+
+	 END DO
+       END DO
+!
+! Compute equation 22
+!
+	 CALC_EP_star = 1.0
+	 DO I = 1, MMAX
+           SUM_LOCAL = 0.0
+         
+	   IF( I .GE. 2) THEN
+	     DO J = 1, (I-1)
+	     
+	       IF( P_IJ(I, J) == EPs_max_TMP(I) ) THEN
+	         SUM_LOCAL = SUM_LOCAL
+	       ELSE
+	         SUM_LOCAL = SUM_LOCAL + (1.0 - EPs_max_TMP(I)/P_IJ(I, J))*COMP_X_I(J)/X_IJ(I, J)
+	       ENDIF
+	   
+	     END DO
+	   ENDIF
+	   
+	   IF( (I+1) .LE. MMAX) THEN  
+	     DO J = (I+1), MMAX
+	       
+	       IF( P_IJ(I, J) == EPs_max_TMP(I) ) THEN
+	         SUM_LOCAL = SUM_LOCAL
+	       ELSE
+	         SUM_LOCAL = SUM_LOCAL + (1.0 - EPs_max_TMP(I)/P_IJ(I, J))*COMP_X_I(J)/X_IJ(I, J)
+	       ENDIF
+
+	     END DO
+	   ENDIF
+	       
+	   IF (SUM_LOCAL .NE. 0.0) THEN
+	     P_IT(I) = EPs_max_TMP(I)/(1.0 - SUM_LOCAL)
+	   ELSE
+	     P_IT(I) = 1.0 ! do nothing if particles have same diameter
+	   ENDIF
+!   
+	   CALC_EP_star = MIN(P_IT(I), CALC_EP_star)
+
+         END DO !for I
+
+! for the case of all phases having same diameter	 
+
+	 IF (CALC_EP_star == 1.0) CALC_EP_star = EPs_max_TMP(1)
+!
+! end modifications by sof (May-02-2005)
+
+!
+! Part implemented by Dinesh for binary mixture, uncomment to use (Sof)
+!
+! 	if ((EP_s(IJK,1)+EP_s(IJK,2)) .NE. 0) THEN
+!	   xbar = EP_s(IJK,1)/(EP_s(IJK,1)+EP_s(IJK,2))
+!
+!	   if (xbar .LE. ep_s_max_ratio(1,2)) THEN
+!	      CALC_EP_star =MAX(0.36d0, (1.-(((ep_s_max(1)-ep_s_max(2))+&
+!              (1.-d_p_ratio(1,2))*(1-ep_s_max(1))*ep_s_max(2))*(ep_s_max(1)+&
+!              (1-ep_s_max(1)) *ep_s_max(2))*xbar/ep_s_max(1)+ep_s_max(2))))
+!     	   else
+!    	      CALC_EP_star =MAX(0.36d0, (1.-((1. -d_p_ratio(1,2))*(ep_s_max(1)&
+!              +(1-ep_s_max(1))*ep_s_max(2))*(1. -xbar) +ep_s_max(1))))
+!	   end if
+!	else
+!	   CALC_EP_star = MIN(ep_s_max(1), ep_s_max(2))
+!	end if
+! 
 	
       RETURN  
       END function CALC_ep_star
