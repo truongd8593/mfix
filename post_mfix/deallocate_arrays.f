f258b8e1c364a8d2a71edd578f2313e5c8d69a25
Sreekanth Pannala <pannalas@ornl.gov>
2006-07-25 18:59:55 +0000

    merged deallocate arrays from interp_res and brought it upto date with mfix allocate arrays

--- post_mfix/deallocate_arrays.f
+++ post_mfix/deallocate_arrays.f
@@ -38,6 +38,7 @@
       Use xsi_array
       Use vshear
       Use mflux
+      Use mchem
       IMPLICIT NONE
       
       INTEGER M
@@ -110,6 +111,9 @@
       Deallocate(  W_so  )
       Deallocate(  P_s  )
       Deallocate(  P_s_c  )
+      Deallocate(  P_s_v  )
+      Deallocate(  P_s_f  )
+      Deallocate(  P_s_p  )
       Deallocate(  P_star  )
       Deallocate(  P_staro  )
       Deallocate(  THETA_m  )
@@ -234,6 +238,25 @@
       
       ENDIF
 
+      Deallocate( N_sh )
+
+!dqmom
+
+      deallocate(  D_p  )
+      deallocate(  D_po )
+      deallocate(  Source_a)
+      deallocate(  S_bar)
+      deallocate(  Matrix_a)
+      deallocate(  Matrix_b)
+      deallocate(  Matrix_c)
+      deallocate(  Inv_a)
+      deallocate(  A)
+      deallocate(  omega)
+      deaLLocate(  beta_a)
+      deaLLocate(  ystart)
+!     deaLLocate(  g_a)
+
+
 !tau_g
       Deallocate(  TAU_U_g )
       Deallocate(  TAU_V_g )
@@ -251,6 +274,8 @@
       Deallocate(  Array4 )
       Deallocate(  Array1i )
       Deallocate(  Array1c )
+      deallocate(  TMP4)
+      deallocate(  ArrayLM)
 
 !tmp_array1
       Deallocate(  Arraym1 )
@@ -273,6 +298,23 @@
       Deallocate(  ALPHA_s  )
       Deallocate(  MU_s_c  )
       Deallocate(  LAMBDA_s_c  )
+      Deallocate(  LAMBDA_s_v )
+      Deallocate(  LAMBDA_s_f )
+      Deallocate(  LAMBDA_s_p )
+      Deallocate(  MU_s_v )
+      Deallocate(  MU_s_f )
+      Deallocate(  MU_s_p )
+      Deallocate(  MU_b_v )
+      Deallocate(  ALPHA_s_v )
+      Deallocate(  ALPHA_s_p )
+      Deallocate(  ALPHA_s_f )
+      Deallocate(  EP_star_array )
+      Deallocate(  EP_g_blend_start )
+      Deallocate(  EP_g_blend_end )
+      Deallocate(  VREL_array )
+      Deallocate(  I2_devD_s )
+      Deallocate(  TrM_s )
+      Deallocate(  TrDM_s )
       
 !xsi_array
       Deallocate(  Xsi_e )
@@ -291,32 +333,91 @@
 !
 
 ! array allocation for higher order implementation
-      Allocate(           FLAG3 (DIMENSION_4) )
-      Allocate(           CELL_CLASS3 (DIMENSION_4) )
-      Allocate(           I3_OF (DIMENSION_4) )
-      Allocate(           J3_OF (DIMENSION_4) )
-      Allocate(           K3_OF (DIMENSION_4) )
-      Allocate(           Im1_3 (-1:DIMENSION_I+1) )
-      Allocate(           Ip1_3 (-1:DIMENSION_I+1) )
-      Allocate(           Jm1_3 (-1:DIMENSION_J+1) )
-      Allocate(           Jp1_3 (-1:DIMENSION_J+1) )
-      Allocate(           Km1_3 (-1:DIMENSION_K+1) )
-      Allocate(           Kp1_3 (-1:DIMENSION_K+1) )
+      Deallocate(           FLAG3 )
+      Deallocate(           CELL_CLASS3 )
+      Deallocate(           I3_OF )
+      Deallocate(           J3_OF )
+      Deallocate(           K3_OF )
+      Deallocate(           Im1_3 )
+      Deallocate(           Ip1_3 )
+      Deallocate(           Jm1_3 )
+      Deallocate(           Jp1_3 )
+      Deallocate(           Km1_3 )
+      Deallocate(           Kp1_3 )
  
 !mflux
-      Allocate(    Flux_gE(DIMENSION_3) ) 
-      Allocate(    Flux_sE(DIMENSION_3, DIMENSION_M) ) 
-      Allocate(    Flux_gN(DIMENSION_3) ) 
-      Allocate(    Flux_sN(DIMENSION_3, DIMENSION_M) ) 
-      Allocate(    Flux_gT(DIMENSION_3) ) 
-      Allocate(    Flux_sT(DIMENSION_3, DIMENSION_M) ) 
-      Allocate(    ROP_gE(DIMENSION_3) ) 
-      Allocate(    ROP_sE(DIMENSION_3, DIMENSION_M) ) 
-      Allocate(    ROP_gN(DIMENSION_3) ) 
-      Allocate(    ROP_sN(DIMENSION_3, DIMENSION_M) ) 
-      Allocate(    ROP_gT(DIMENSION_3) ) 
-      Allocate(    ROP_sT(DIMENSION_3, DIMENSION_M) ) 
+      Deallocate(    Flux_gE)
+      Deallocate(    Flux_sE)
+      Deallocate(    Flux_gN)
+      Deallocate(    Flux_sN)
+      Deallocate(    Flux_gT)
+      Deallocate(    Flux_sT)
+      Deallocate(    ROP_gE)
+      Deallocate(    ROP_sE)
+      Deallocate(    ROP_gN)
+      Deallocate(    ROP_sN)
+      Deallocate(    ROP_gT)
+      Deallocate(    ROP_sT)
 
+!spill over from interp_res.f
+
+        deallocate( ijksize3_all )
+        deallocate( ijkstart3_all )
+        deallocate( ijkend3_all )
+
+        deallocate( ijksize4_all )
+        deallocate( ijkstart4_all )
+        deallocate( ijkend4_all )
+
+        deallocate( istart_all)
+        deallocate( jstart_all )
+        deallocate( kstart_all )
+
+        deallocate( istart1_all )
+        deallocate( jstart1_all )
+        deallocate( kstart1_all )
+
+        deallocate( istart2_all )
+        deallocate( jstart2_all )
+        deallocate( kstart2_all )
+
+        deallocate( istart3_all )
+        deallocate( jstart3_all )
+        deallocate( kstart3_all )
+
+        deallocate( istart4_all )
+        deallocate( jstart4_all )
+        deallocate( kstart4_all )
+
+        deallocate( iend_all )
+        deallocate( jend_all )
+        deallocate( kend_all )
+
+        deallocate( iend1_all )
+        deallocate( jend1_all )
+        deallocate( kend1_all )
+
+        deallocate( iend2_all )
+        deallocate( jend2_all )
+        deallocate( kend2_all )
+
+        deallocate( iend3_all )
+        deallocate( jend3_all )
+        deallocate( kend3_all )
+
+        deallocate( iend4_all )
+        deallocate( jend4_all )
+        deallocate( kend4_all )
+
+        deallocate( displs )
+
+        deallocate( imap)
+        deallocate( jmap )
+        deallocate( kmap )
+
+        deallocate( imap_c )
+        deallocate( jmap_c )
+        deallocate( kmap_c )
 
      
       RETURN
