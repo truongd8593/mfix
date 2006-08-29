14d7105247a4105ce448e811e6db14c30f4be47b
Madhava Syamlal <madhava.syamlal@netl.doe.gov>
2006-08-29 17:58:19 +0000

    The directories a and b under reactor1 were moved to the upper directory so that these cases can be added to the test harness.

---
+++ tutorials/reactor1b/rrates.f
@@ -0,0 +1,247 @@
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
+!                                                                      C
+!  Module name: RRATES(IER)                                            C
+!  Purpose: Calculate reaction rates for various reactions in cell ijk C
+!                                                                      C
+!  Author:                                            Date:            C
+!  Reviewer:                                          Date:            C
+!                                                                      C
+!  Revision Number:                                                    C
+!  Purpose:                                                            C
+!  Author:                                            Date: dd-mmm-yy  C
+!  Reviewer:                                          Date: dd-mmm-yy  C
+!                                                                      C
+!  Literature/Document References:                                     C
+!  O. Levenspiel, "Chemical Reaction Engineering," Wiley Eastern,   
+!       New Delhi (1974)                                        
+!                                                                      C
+!                                                                      C
+!                                                                      C
+!  Local variables:                                                    C
+!                                                                      C
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
+!
+!
+      SUBROUTINE RRATES(IER) 
+!...Translated by Pacific-Sierra Research VAST-90 2.06G5  12:17:31  12/09/98  
+!...Switches: -xf
+!-----------------------------------------------
+!   M o d u l e s 
+!-----------------------------------------------
+      USE param 
+      USE param1 
+      USE parallel 
+      USE fldvar
+      USE rxns
+      USE energy
+      USE geometry
+      USE run
+      USE indices
+      USE physprop
+      USE constant
+      USE funits 
+      USE compar        !//d
+      USE sendrecv      !// 400
+      IMPLICIT NONE
+!-----------------------------------------------
+!   G l o b a l   P a r a m e t e r s
+!-----------------------------------------------
+!-----------------------------------------------
+!   D u m m y   A r g u m e n t s
+!-----------------------------------------------
+!
+!                      Error index
+      INTEGER          IER
+!
+!                      Local phase and species indices
+      INTEGER          L, LM, M, N
+
+!                      cell index
+      INTEGER          IJK
+      
+      DOUBLE PRECISION R_tmp(0:MMAX, 0:MMAX)
+      DOUBLE PRECISION RXNA
+!
+!-----------------------------------------------
+      INCLUDE 'function.inc'
+      
+      IER =0
+      R_tmp = UNDEFINED
+!
+!  ---  Remember to include all the local variables here for parallel
+!  ---- processing
+!$omp  parallel do private(ijk, R_tmp, L, LM, M, N)
+
+      DO IJK = IJKSTART3, IJKEND3 
+      
+         IF (FLUID_AT(IJK)) THEN 
+!
+!
+!  User input is required in sections 1 through 4.
+!
+!1111111111111111111111111111111111111111111111111111111111111111111111111111111
+!
+! 1. Write the rates of various reactions:
+!    Write the reaction rates for each of the reactions as RXNxF and RXNxB (both
+!    quantities >= 0), where x identifies the reaction, F stands for forward
+!    rate, and B stands for the backward rate.  The rates can be in
+!    g-mole/(cm^3.s) or g/(cm^3.s).  For the sake of clarity, give the reaction
+!    scheme and the units in a comment statement above the rate expression.
+!    The volume (cm^3) is that of the computational cell.  Therefore, for
+!    example, the rate term of a gas phase reaction will have a multiplicative
+!    factor of epsilon. Note that X_g and X_s are mass fractions
+!
+!              No   1   2    3
+!     GAS Species   A,  R, Inert
+
+!  a)   A -> 3R
+!       Levenspiel (p. 111, Example 4)
+!            converting the rate expression given by Levenspiel for use here
+!           -rA = 1E-2 * C_A^0.5 g-mol/L.s    (C_A is in mol/L)
+!               = 1E-3 * 1E-2 * C_A^0.5 g-mol/cm^3.s
+!               = EP_g * 1E-3 * 1E-2 * C_A^0.5 g-mol/cm^3.s (in reactor volume)
+!               = EP_g * 1E-3 * 1E-2 * C_A^0.5 g-mol/cm^3.s (in reactor volume)
+!               = EP_g * 1E-3 * 1E-2 * (1.E3*RO_g*X_A/M_A)^0.5 g-mol/cm^3.s (in reactor volume)
+
+        Rxna = EP_g(IJK) * C(1) * (RO_g(IJK)*X_g(IJK,1)/Mw_g(1))**C(2)
+
+!
+!
+!2222222222222222222222222222222222222222222222222222222222222222222222222222222
+!
+! 2. Write the formation and consumption rates of various species:
+!    Obtain the rates of formation and consumption of various species
+!    in g/(cm^3.s) from the rate expressions RXNxF and RXNxB obtained in the
+!    previous section.  Pay attention to the units of RXNxF and RXNxB.
+!    the formation rates for gas species n are added to get R_gp (IJK, n).
+!    All the consumption rates are added and then divided by X_g(IJK, n) to
+!    get RoX_gc(IJK, n).  If X_g(IJK, n) is zero and species n is likely
+!    to be consumed in a reaction then it is recommended that RoX_gc (IJK, n)
+!    be initialized to the derivative of the consumption rate with respect to
+!    X_g at X_g=0.
+!    If the slope is not known analytically a small value such as 1.0e-9 may
+!    instead be used.  A similar procedure is used for all the species in the
+!    solids phases also.
+
+!  (1) A
+      R_gp(IJK, 1) = ZERO
+      IF(X_g(IJK, 1) .GT. ZERO) THEN
+        RoX_gc(IJK, 1) = RXNA  * MW_g(1) / X_g(IJK, 1)
+      ELSE
+        RoX_gc(IJK, 1) = 1.0e-9
+      ENDIF
+
+!  (2) R
+      R_gp(IJK, 2) = 3. * RXNA * MW_g(2)
+      RoX_gc(IJK, 2) = ZERO
+
+!  (3) Inert
+      R_gp(IJK, 3) = ZERO
+      RoX_gc(IJK, 3) = ZERO
+
+!
+!  GAS SPECIES
+!
+!
+!  SOLIDS SPECIES
+!
+!
+!3333333333333333333333333333333333333333333333333333333333333333333333333333333
+!
+! 3.  Determine the g/(cm^3.s) transferred from one phase to the other.
+!          R_tmp(To phase #, From phase #)
+!     e.g. R_tmp(0,1) -  mass generation of gas phase from solids-1,
+!          R_tmp(0,2) -  mass generation of gas phase from solids-2,
+!          R_tmp(1,0) -  mass generation of solid-1 from gas = -R_tmp(0,1)
+!          R_tmp(1,2) -  mass generation of solid-1 from solids-2.
+!     Note, for example, that if gas is generated from solids-1 then
+!     R_tmp(0,1) > 0.
+!     The R-phase matrix is skew-symmetric and diagonal elements are not needed.
+!     Only one of the two skew-symmetric elements -- e.g., R_tmp(0,1) or
+!     R_tmp(1,0) -- needs to be specified.
+!
+!
+      if(MMAX > 0) R_tmp(0,1) =  ZERO
+!
+!4444444444444444444444444444444444444444444444444444444444444444444444444444444
+!
+! 4.  Determine the heat of reactions in cal/(cm^3.s) at the
+!     temperature T_g or T_s1.  Note that for exothermic reactions
+!     HOR_g (or HOR_s) will be negative. The assignment of heat of reaction
+!     is user defined as it depends upon the microphysics near the interface,
+!     which is averaged out in the multiphase flow equations.  For example,
+!     heat of Reaction for the C + O2 reaction is split into parts;
+!     CO formation is assigned to the solid phase and CO2 formation from CO to
+!     the gas phase.
+!
+!
+!==============================================================================
+!
+!     No user input is required below this line
+!-----------------------------------------------------------------------------
+!   Determine g/(cm^3.s) of mass generation for each of the phases by adding
+!   the reaction rates of all the individual species.
+
+            SUM_R_G(IJK) = ZERO 
+            IF (SPECIES_EQ(0)) THEN 
+               IF (NMAX(0) > 0) THEN 
+                  SUM_R_G(IJK) = SUM_R_G(IJK) + SUM(R_GP(IJK,:NMAX(0))-ROX_GC(&
+                     IJK,:NMAX(0))*X_G(IJK,:NMAX(0))) 
+               ENDIF
+	    ELSE
+	      DO M = 1, MMAX
+	        IF(R_tmp(0,M) .NE. UNDEFINED)THEN
+		  SUM_R_G(IJK) = SUM_R_G(IJK) + R_tmp(0,M)
+		ELSEIF(R_tmp(M,0) .NE. UNDEFINED)THEN
+		  SUM_R_G(IJK) = SUM_R_G(IJK) - R_tmp(M,0)
+		ENDIF
+	      ENDDO 
+            ENDIF 
+!
+            DO M = 1, MMAX 
+               SUM_R_S(IJK,M) = ZERO 
+               IF (SPECIES_EQ(M)) THEN 
+                  IF (NMAX(M) > 0) THEN 
+                     SUM_R_S(IJK,M) = SUM_R_S(IJK,M) + SUM(R_SP(IJK,M,:NMAX(M))&
+                        -ROX_SC(IJK,M,:NMAX(M))*X_S(IJK,M,:NMAX(M))) 
+                  ENDIF 
+	       ELSE
+ 	         DO L = 0, MMAX
+	           IF(R_tmp(M,L) .NE. UNDEFINED)THEN
+		     SUM_R_s(IJK,M) = SUM_R_s(IJK,M) + R_tmp(M,L)
+		   ELSEIF(R_tmp(L,M) .NE. UNDEFINED)THEN
+		     SUM_R_s(IJK,M) = SUM_R_s(IJK,M) - R_tmp(L,M)
+		   ENDIF
+	         ENDDO 
+               ENDIF 
+            END DO 
+	    
+!
+!
+!     Store R_tmp values in an array.  Only store the upper triangle without
+!     the diagonal of R_tmp array.
+!
+            DO L = 0, MMAX 
+               DO M = L + 1, MMAX 
+                  LM = L + 1 + (M - 1)*M/2 
+                  IF (R_TMP(L,M) /= UNDEFINED) THEN 
+                     R_PHASE(IJK,LM) = R_TMP(L,M) 
+                  ELSE IF (R_TMP(M,L) /= UNDEFINED) THEN 
+                     R_PHASE(IJK,LM) = -R_TMP(M,L) 
+                  ELSE 
+                     CALL START_LOG 
+                     IF(DMP_LOG)WRITE (UNIT_LOG, 1000) L, M 
+                     CALL END_LOG 
+                     call mfix_exit(myPE)  
+                  ENDIF 
+               END DO 
+            END DO 
+	   
+         ENDIF 
+      END DO 
+
+ 1000 FORMAT(/1X,70('*')//' From: RRATES',/&
+         ' Message: Mass transfer between phases ',I2,' and ',I2,&
+         ' (R_tmp) not specified',/1X,70('*')/) 
+      RETURN  
+      END SUBROUTINE RRATES 
