458a6172e621f91220d3e2508c5e1be65a043e23
Sofiane Benyahia <sofiane.benyahia@netl.doe.gov>
2006-12-01 15:16:11 +0000

    Duplicate code was commented. Code doesn't serve any purpose for ISAT-CHEM.

--- model/check_data_20.f
+++ model/check_data_20.f
@@ -346,17 +346,17 @@
                     ENDIF  
 		  ENDIF ! for MMAX > 0
 !
-!     CHEM & ISAT begin (nan xie)
-                  IF ((RUN_TYPE /= 'NEW') .AND. (CALL_ISAT .OR. CALL_DI))THEN
-                  ELSE
-                    IF (ABS(DIF) > small_number) THEN
-                       IF (.NOT.ABORT) THEN 
-                          IF(DMP_LOG)WRITE (UNIT_LOG, 1050) 
-                          ABORT = .TRUE. 
-                       ENDIF 
-                       IF(DMP_LOG)WRITE (UNIT_LOG, 1100) I, J, K, (1.- dif) 
-                    ENDIF 
-                  END IF
+!     CHEM & ISAT begin (nan xie) Commented by sof, because this is a duplication of line 320.
+!                  IF ((RUN_TYPE /= 'NEW') .AND. (CALL_ISAT .OR. CALL_DI))THEN
+!                  ELSE
+!                    IF (ABS(DIF) > small_number) THEN
+!                       IF (.NOT.ABORT) THEN 
+!                          IF(DMP_LOG)WRITE (UNIT_LOG, 1050) 
+!                          ABORT = .TRUE. 
+!                       ENDIF 
+!                       IF(DMP_LOG)WRITE (UNIT_LOG, 1100) I, J, K, (1.- dif) 
+!                    ENDIF 
+!                  END IF
 !     CHEM & ISAT end (nan xie)
 !
 !  Check whether L_scale is non-zero anywhere
