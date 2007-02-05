a53d6b4b255ffee63e7d455f42dd77540a2208af
Madhava Syamlal <madhava.syamlal@netl.doe.gov>
2007-02-05 13:40:24 +0000

    Adding a new parallel benchmark case on char combustion.

---
+++ tutorials/ParallelBenchmarkCases/C_COM_06/chem/calc_jacobian.f
@@ -0,0 +1,75 @@
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
+!                                                                         C  
+!     Module name: CALC_JACOBIAN                                          C
+!     Purpose: Provide the Jacobian matrix of source terms of ODEs        C
+!                                                                         C
+!     Author: Nan Xie                                   Date: 02-Aug-04   C
+!     Reviewer:                                         Date:             C
+!                                                                         C
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
+!
+      SUBROUTINE CALC_JACOBIAN(Y, NX, Ja, IJK)
+!
+!-----------------------------------------------
+!   M o d u l e s 
+!-----------------------------------------------
+      USE param1
+!      USE usr
+      USE mchem
+      IMPLICIT NONE
+!-----------------------------------------------
+!     G l o b a l   P a r a m e t e r s
+!-----------------------------------------------
+!-----------------------------------------------
+!     L o c a l   P a r a m e t e r s
+!-----------------------------------------------
+!-----------------------------------------------
+!     L o c a l   V a r i a b l e s
+!----------------------------------------------- 
+!
+!                      Number of odes
+      INTEGER          NX
+!
+      INTEGER          IJK
+!
+!                      Values of odes
+      DOUBLE PRECISION Y(NX)
+!
+!                      Jacobian matrix 
+      DOUBLE PRECISION Ja(NX,NX)
+!  
+!                      ADIFOR
+      DOUBLE PRECISION G_X(NX,NX), G_Y(NX,NX),Y_source(NX)
+!                     
+!                      Loop indices
+      INTEGER          NL, NM
+
+!
+!     Using ADIFOR to calulate the Jacobian 
+!              
+      DO NL = 1, NX
+         DO NM = 1, NX
+            G_X(NL, NM) = ZERO
+         END DO
+      END DO
+
+      DO NL = 1, NX
+         DO NM = 1, NX
+            IF (NL .EQ. NM) THEN
+               G_X(NL, NM) = ONE
+            END IF
+         END DO
+      END DO   
+      
+      CALL G_DERIVS(NX, Y, G_X, NX, Y_source, G_Y, NX, N_sh(IJK,1))
+!
+!     transport g_y
+!
+      DO NL = 1, NX
+         DO NM = 1, NX
+            Ja(NM, NL) = G_Y(NL, NM)
+         END DO
+      END DO
+
+      RETURN
+      END 
---
+++ tutorials/ParallelBenchmarkCases/C_COM_06/usr0.f
@@ -0,0 +1,75 @@
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
+!                                                                      C
+!  Module name: USR0                                                   C
+!  Purpose: This routine is called before the time loop starts and is  C
+!           user-definable.  The user may insert code in this routine  C
+!           or call appropriate user defined subroutines.  This        C
+!           can be used for setting constants and checking errors in   C
+!           data.  This routine is not called from an IJK loop, hence  C
+!           all indices are undefined.                                 C
+!                                                                      C
+!  Author:                                            Date: dd-mmm-yy  C
+!  Reviewer:                                          Date: dd-mmm-yy  C
+!                                                                      C
+!  Revision Number:                                                    C
+!  Purpose:                                                            C
+!  Author:                                            Date: dd-mmm-yy  C
+!  Reviewer:                                          Date: dd-mmm-yy  C
+!                                                                      C
+!  Literature/Document References:                                     C
+!                                                                      C
+!  Variables referenced:                                               C
+!  Variables modified:                                                 C
+!                                                                      C
+!  Local variables:                                                    C
+!                                                                      C
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
+!
+      SUBROUTINE USR0 
+!...Translated by Pacific-Sierra Research VAST-90 2.06G5  12:17:31  12/09/98  
+!...Switches: -xf
+      USE param 
+      USE param1 
+      USE physprop
+      USE constant
+      USE funits    
+      Use usr
+      IMPLICIT NONE
+      INCLUDE 'usrnlst.inc' 
+!-----------------------------------------------
+!
+!  Include files defining common blocks here
+!
+!
+!  Define local variables here
+!
+      DOUBLE PRECISION SUM
+      LOGICAL COMPARE
+!
+!  Include files defining statement functions here
+!
+!
+!  Insert user-defined code here
+!
+
+!     allocate array declared in usrnlst.inc
+      Allocate(  N_Sh (DIMENSION_3, DIMENSION_M) )
+      
+        IF(PAFC .EQ. UNDEFINED ) &
+          CALL ERROR_ROUTINE ('USR0', 'PAFC not specified', 1, 1)
+!
+        IF(PAA .NE. UNDEFINED)THEN
+          SUM = PAFC + PAA
+          IF( .NOT.COMPARE(ONE,SUM) )THEN
+            WRITE(UNIT_LOG,'(A,F10.5/A)') &
+              ' *** PAFC + PAA = ',SUM,' It should be equal to 1.0'
+            CALL EXIT
+          ENDIF
+        ELSE
+          PAA = 1.0 - PAFC
+        ENDIF
+!
+!  Function of the ash-layer void fraction
+      f_EP_A = (0.25 + 0.75 * ( 1.0 - PAA )) ** 2.5
+      RETURN  
+      END SUBROUTINE USR0 
