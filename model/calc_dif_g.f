b29da53c212d9d9d6f75f36987428c81636cc934
Madhava Syamlal <madhava.syamlal@netl.doe.gov>
2007-04-23 12:52:38 +0000

    Added influence of gas temperature and gas pressure from Fuller relation (N. Reuge).

--- model/calc_dif_g.f
+++ model/calc_dif_g.f
@@ -5,7 +5,11 @@
 !                                                                      C
 !  Author:M. Syamlal                                  Date: 13-FEB-98  C
 !  Reviewer:                                          Date: dd-mmm-yy  C
-!                                                                      C
+!           
+!  Updated with the dilute mixture approximation for calculation of    C
+!  multicomponent diffusion coefficients                               C
+!  Author:N. Reuge                                    Date: 11-APR-07  C
+!  Reviewer:                                          Date: dd-mmm-yy  C
 !                                                                      C
 !  Literature/Document References:                                     C
 !                                                                      C
@@ -48,32 +52,109 @@
       INTEGER          IER
 !
 !                      Indices
-      INTEGER          IJK, N
+      INTEGER          IJK, N, N1, N2
       
-      DOUBLE PRECISION	Dab
+      DOUBLE PRECISION Dab(3,3), SUMJ, Tg0, Pg0
 !
 !-----------------------------------------------
       INCLUDE 'function.inc'
 !
 !
-!     Gas diffusion coefficient
+!     Default gas diffusion coefficient
 !     Bird, Stewart, and Lightfoot (1960) -- CO2--N2 at 298.2 K
-      Dab = 0.165D0       !cm^2/s
-      IF(UNITS == 'SI') Dab = Dab*0.0001D0   !m^2/s
+      Dab(1,2) = 0.165D0       !cm^2/s
+      Tg0 = 298.2
+      Pg0 = 1.01e6
+!
+      IF(UNITS == 'SI') THEN
+         Dab(1,2) = Dab(1,2)*0.0001D0   !m^2/s
+	 Pg0 = Pg0/10.D0                !Pa
+      ENDIF
+
+
+!********Gas diffusion coef for a system of 3 species***************
+!
+!     Calculated using relation derived from Chapman and Enskog's
+!     theory of gases - Reid, Prausnitz and Poling (1987)
+
+!     Binary diffusion coefficient SiH4/H2 at 873 K
+!      Dab(1,2) = 3.78      ! cm^2/s
+!	Dab(2,1) = Dab(1,2)
+
+!     Binary diffusion coefficient SiH4/N2 at 873 K
+!      Dab(1,3) = 1.02      ! cm^2/s
+!	Dab(3,1) = Dab(1,3)
+
+!     Binary diffusion coefficient H2/N2 at 873 K
+!      Dab(2,3) = 4.52      ! cm^2/s
+!	Dab(3,2) = Dab(2,3)
+
+!      Tg0 = 873.0
+!	Pg0 = 1.01e6
+
+
+!      IF(UNITS == 'SI') THEN
+!	  DO N1 = 1, NMAX(0)-1
+!	    DO N2 = N1+1, NMAX(0)
+!	      Dab(N1,N2) = Dab(N1,N2)*0.0001D0   !m^2/s
+!	      Dab(N2,N1) = Dab(N1,N2)
+!	    END DO
+!        END DO
+!        Pg0 = Pg0/10.D0                    !Pa
+!	ENDIF
+
+!*******************************************************************
+
 !
       IF (DIF_G0 /= UNDEFINED) RETURN  
+
+
 !!$omp  parallel do private(ijk) &
 !!$omp& schedule(dynamic,chunk_size)
+
+!     Default calculation of diffusivities
+!     Influence of gas temperature and gas pressure from Fuller relation
+!
       DO N = 1, NMAX(0) 
          DO IJK = IJKSTART3, IJKEND3 	 
             IF (FLUID_AT(IJK)) THEN 
-               DIF_G(IJK,N) = ROP_G(IJK)*Dab 
+               DIF_G(IJK,N) = ROP_G(IJK)*Dab(1,2)*(T_g(IJK)/Tg0)**1.75&
+               *Pg0/P_g(IJK)
             ELSE 
                DIF_G(IJK,N) = ZERO 
             ENDIF 
          END DO 
       END DO 
          
+
+!***************************************************************************
+!     Caculation of diffusivities using the dilute mixture approximation for 
+!     multicomponent diffusion - ref : Curtiss-Hirschfelder / Wilke / Blanc
+!     Valid if the mass fraction of the carrier species > 0.9
+!     Influence of gas temperature and gas pressure from Fuller relation
+!
+
+!      DO N = 1, NMAX(0) 
+!         DO IJK = IJKSTART3, IJKEND3 	 
+!           IF (FLUID_AT(IJK)) THEN 
+!	       IF ((1.0-X_g(IJK,N)) > 1.e-8) THEN
+!       	     SUMJ = ZERO      
+!	         DO N2 = 1, NMAX(0)
+!	           IF (N2 /= N) SUMJ = SUMJ+X_g(IJK,N2)/Dab(N,N2)
+!	         END DO 
+!               DIF_G(IJK,N) = ROP_G(IJK)*(1-X_g(IJK,N))/SUMJ&
+!	         *(T_g(IJK)/Tg0)**1.75*Pg0/P_g(IJK)
+!	       ELSE
+!	         DIF_G(IJK,N) = ROP_G(IJK)*Dab(1,2) 
+!	       END IF
+!           ELSE 
+!               DIF_G(IJK,N) = ZERO 
+!           ENDIF 
+!         END DO 
+!      END DO 
+!****************************************************************************
+
+
       RETURN  
       END SUBROUTINE CALC_DIF_G 
 
