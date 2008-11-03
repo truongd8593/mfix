0e345bf9920d71d66cb8ec0c51cc7bc9e345dac6
Sofiane Benyahia <sofiane.benyahia@netl.doe.gov>
2008-11-03 22:24:51 +0000

    Corrected bug in des_theta calculation.

--- model/des/des_granular_temperature.f
+++ model/des/des_granular_temperature.f
@@ -60,18 +60,18 @@
                NPG = 0
             Endif
             
+            TEMP = ZERO
             DO IPART = 1, NPG 
                NP = PIC(I,J,K)%p(IPART)
                M = PIJK(NP,5)
                
-               TEMP = ZERO
-               TEMP = (DES_VEL_NEW(NP,1)-DES_U_s(IJK,M))**2 
+               TEMP = TEMP + (DES_VEL_NEW(NP,1)-DES_U_s(IJK,M))**2 
                TEMP = TEMP + (DES_VEL_NEW(NP,2)-DES_V_s(IJK,M))**2
                IF(DIMN.EQ.3) THEN 
                   TEMP = TEMP + (DES_VEL_NEW(NP,3)-DES_W_s(IJK,M))**2     
                END IF
-               DES_THETA(IJK,M) = TEMP/3.0d0
             end DO
+            IF(NPG>0)  DES_THETA(IJK,M) = TEMP/(3.0d0 * DFLOAT(NPG))
          end IF
          
       end DO
@@ -81,10 +81,12 @@
 !         WRITE(17,*)' '
 !         WRITE(17,*)'T="',S_TIME,'"'
 !         DO IJK = IJKSTART3, IJKEND3
-!            I = I_OF(IJK)
-!            J = J_OF(IJK)
-!            K = k_OF(IJK)
-!            WRITE(17,*) IJK, I, J, K, DES_THETA(IJK,1)
+!           IF(FLUID_AT(IJK)) THEN
+!             I = I_OF(IJK)
+!             J = J_OF(IJK)
+!             K = k_OF(IJK)
+!             WRITE(17,*) IJK, I, J, K, DES_THETA(IJK,1)
+!           ENDIF
 !         END DO
 
       RETURN
