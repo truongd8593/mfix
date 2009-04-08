e0d1ef782b721c27660b50476685897744f6f4e9
Janine Carney <janine.carney@netl.doe.gov>
2009-04-08 21:53:51 +0000

    general cleanup

--- model/des/gas_drag.f
+++ model/des/gas_drag.f
@@ -49,71 +49,68 @@
 !     
 !     Error index
       INTEGER          IER
-!     
+     
 !     Indices
-      INTEGER          IJK, NC
-!     
+      INTEGER          IJK, I, J, K, NC
+     
 !     Phase index
       INTEGER          M, UV, VV, WV
+
 !     Averaging Factor 
       DOUBLE PRECISION :: AVG_FACTOR
-!     Grid indices
-      INTEGER          I,J,K
-!     
+
       DOUBLE PRECISION USFCM, VSFCM, WSFCM, VCELL
       DOUBLE PRECISION A_M(DIMENSION_3, -3:3, 0:DIMENSION_M)
       DOUBLE PRECISION B_M(DIMENSION_3, 0:DIMENSION_M)
       DOUBLE PRECISION VXF_GS(DIMENSION_3, DIMENSION_M)  
-      Logical DES_ON_TMP
       DOUBLE PRECISION tmp_A, tmp_B
- !     
+     
 !-----------------------------------------------
       INCLUDE 'function.inc'
       INCLUDE 'fun_avg1.inc'
       INCLUDE 'fun_avg2.inc'
+
       AVG_FACTOR = 0.25D0*(DIMN-2) + 0.5D0*(3-DIMN)
-	DES_ON_TMP = DES_INTERP_ON
+
       IF(UV.EQ.1) THEN
          DO M = 1, MMAX
             DO IJK = IJKSTART3, IJKEND3
                IF(FLUID_AT(IJK)) THEN
-                  if(DES_ON_TMP) then 
-                     I = I_OF(IJK)
-                     J = J_OF(IJK)
-                     K = K_OF(IJK)
-                     
+                  I = I_OF(IJK)
+                  J = J_OF(IJK)
+                  K = K_OF(IJK)
+
+                  IF(DES_INTERP_ON) then 
                      tmp_A =  - AVG_FACTOR*(DRAG_AM(I&
                           &,J,K,M) + DRAG_AM(I,J-1,K,M))
                      tmp_B =  - AVG_FACTOR*(DRAG_BM(I&
                           &,J,K,1,M) + DRAG_BM(I,J-1,K,1,M))
                          
-                     IF(DIMN.eq.3) then 
+                     IF(DIMN.eq.3) THEN 
                         tmp_A = tmp_A - AVG_FACTOR*(DRAG_AM(I&
                              &,J,K-1,M) + DRAG_AM(I,J-1,K-1,M))
                         tmp_B = tmp_B - AVG_FACTOR*(DRAG_BM(I&
                              &,J,K-1,1,M) + DRAG_BM(I,J-1,K-1,1,M))
-                     end IF
+                     ENDIF
                      
                      VCELL = VOL(IJK)
-                     
                      tmp_A = tmp_A*VCELL
                      tmp_B = tmp_B*VCELL
                      
-                     else 
-                        USFCM = AVG_X(DES_U_S(IJK,M),DES_U_S(EAST_OF(IJK),M),I_OF(IJK))
-                        tmp_A =  - VXF_GS(IJK,M)
-                        tmp_B =  - VXF_GS(IJK,M)*USFCM
-                     end if
+                  ELSE 
+                     USFCM = AVG_X(DES_U_S(IJK,M),DES_U_S(EAST_OF(IJK),M),I)
+                     tmp_A =  - VXF_GS(IJK,M)
+                     tmp_B =  - VXF_GS(IJK,M)*USFCM
+                  ENDIF
                      
-                     A_M(IJK,0,0) = A_M(IJK,0,0) + tmp_A
-                     B_M(IJK,0) = B_M(IJK,0) + tmp_B
-               END IF
-            END DO
-         END DO
+                  A_M(IJK,0,0) = A_M(IJK,0,0) + tmp_A
+                  B_M(IJK,0) = B_M(IJK,0) + tmp_B
 
+               ENDIF
+            ENDDO
+         ENDDO
 
-      ELSE IF(VV.EQ.1) THEN
-
+      ELSEIF(VV.EQ.1) THEN
          DO M = 1, MMAX
             DO IJK = IJKSTART3, IJKEND3
                IF(FLUID_AT(IJK)) THEN
@@ -121,45 +118,35 @@
                   J = J_OF(IJK)
                   K = K_OF(IJK)
                   
-                  if(DES_ON_TMP) then 
-                     
-                 tmp_A =  - AVG_FACTOR*(DRAG_AM(I&
-                      &,J,K,M) + DRAG_AM(I-1,J,K,M))
-                 tmp_B =  - AVG_FACTOR*(DRAG_BM(I&
-                      &,J,K,2,M) + DRAG_BM(I-1,J,K,2,M))
+                  IF(DES_INTERP_ON) THEN                      
+                     tmp_A =  - AVG_FACTOR*(DRAG_AM(I&
+                          &,J,K,M) + DRAG_AM(I-1,J,K,M))
+                     tmp_B =  - AVG_FACTOR*(DRAG_BM(I&
+                          &,J,K,2,M) + DRAG_BM(I-1,J,K,2,M))
                  
-                 IF(DIMN.eq.3) then 
-                    tmp_A = tmp_A - AVG_FACTOR*(DRAG_AM(I&
-                         &,J,K-1,M) + DRAG_AM(I-1,J,K-1,M))
-                    tmp_B = tmp_B - AVG_FACTOR*(DRAG_BM(I&
-                         &,J,K-1,2,M) + DRAG_BM(I-1,J,K-1,2,M))
-                 end IF
-                 
-                 VCELL = VOL(IJK)
+                     IF(DIMN.eq.3) THEN
+                        tmp_A = tmp_A - AVG_FACTOR*(DRAG_AM(I&
+                             &,J,K-1,M) + DRAG_AM(I-1,J,K-1,M))
+                        tmp_B = tmp_B - AVG_FACTOR*(DRAG_BM(I&
+                             &,J,K-1,2,M) + DRAG_BM(I-1,J,K-1,2,M))
+                     ENDIF
 
-                                      
-                 tmp_A = tmp_A*VCELL
-                 tmp_B = tmp_B*VCELL
+                     VCELL = VOL(IJK)
+                     tmp_A = tmp_A*VCELL
+                     tmp_B = tmp_B*VCELL
                  
-                 else 
-                    I = I_OF(IJK)
-                    J = J_OF(IJK)
-                    K = K_OF(IJK)
-                    
-                    VCELL = VOL(IJK)
-                    VSFCM = AVG_Y(DES_V_S(IJK,M),DES_V_S(NORTH_OF(IJK),M),J_OF(IJK))
+                  ELSE 
+                    VSFCM = AVG_Y(DES_V_S(IJK,M),DES_V_S(NORTH_OF(IJK),M),J)
                     tmp_A =  - VXF_GS(IJK,M)
                     tmp_B =  - VXF_GS(IJK,M)*VSFCM
-                    
-                 end if
-                 A_M(IJK,0,0) = A_M(IJK,0,0) + tmp_A
-                 B_M(IJK,0) = B_M(IJK,0) + tmp_B
+                  ENDIF
+
+                  A_M(IJK,0,0) = A_M(IJK,0,0) + tmp_A
+                  B_M(IJK,0) = B_M(IJK,0) + tmp_B
                  
-               END IF
-             END DO
-          END DO
-90                  FORMAT(3(1x,i2),10(2x,E12.5))
-          close(900)
+               ENDIF
+             ENDDO
+          ENDDO
 
       ELSE IF(WV.EQ.1) THEN
          DO M = 1, MMAX
@@ -169,8 +156,7 @@
                   J = J_OF(IJK)
                   K = K_OF(IJK)
                   
-                  if(DES_ON_TMP) then 
-                     
+                  IF(DES_INTERP_ON) THEN
                      tmp_A =  - 0.25d0*(DRAG_AM(I&
                           &,J,K,M) + DRAG_AM(I-1,J,K,M)+DRAG_AM(I,J-1,K&
                           &,M)+DRAG_AM(I-1,J-1,K,M))
@@ -179,24 +165,23 @@
                           &-1,K,3,M)+ DRAG_BM(I-1,J-1,K,3,M))
                      
                      VCELL = VOL(IJK)
-                                                           
                      tmp_A = tmp_A*VCELL
                      tmp_B = tmp_B*VCELL
 
-                  else 
-                     WSFCM = AVG_Z(DES_W_S(IJK,M),DES_W_S(TOP_OF(IJK),M),K_OF(IJK))
+                  ELSE 
+                     WSFCM = AVG_Z(DES_W_S(IJK,M),DES_W_S(TOP_OF(IJK),M),K)
                      tmp_A = - VXF_GS(IJK,M)
                      tmp_B = - VXF_GS(IJK,M)*WSFCM
                      
-                  end if
+                  ENDIF
+
                   A_M(IJK,0,0) = A_M(IJK,0,0) + tmp_A
                   B_M(IJK,0) = B_M(IJK,0) + tmp_B
                   
-               END IF
-            END DO
-         END DO
-         
-      END IF
-      !stop
+               ENDIF
+            ENDDO
+         ENDDO
+      ENDIF
+
       RETURN
       END SUBROUTINE GAS_DRAG
