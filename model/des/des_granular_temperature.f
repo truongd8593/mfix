b692bbd6b336089bf3b6637c714d29b2e5720152
Janine Carney <janine.carney@netl.doe.gov>
2010-01-28 21:26:10 +0000

    added comment and modification to global_gran_temp/gran_energy calculation

--- model/des/des_granular_temperature.f
+++ model/des/des_granular_temperature.f
@@ -34,10 +34,8 @@
       INTEGER I, J, K, IJK
 ! 
       INTEGER M, NP, NPG, LL
-! counter for no. of particles in phase m      
-      INTEGER NPG_PHASE(MMAX)
-! temporary variable for mth phase granular temperature
-      DOUBLE PRECISION TEMP(MMAX)
+! temporary variable for granular temperature      
+      DOUBLE PRECISION  TEMP
 ! accounted for particles
       INTEGER PC             
 ! squared particle velocity v.v
@@ -49,8 +47,12 @@
       INCLUDE 'fun_avg2.inc'
 
 
-! Calculate a local species granular temperature at current
-! instant of time
+! Calculate a local granular temperature for current instant of time.
+! Note that the definition of granular temperature below is that of a 
+! species granular temperature wwhere the flutuating particle velocity 
+! is taken as the difference between a particles velocity and the 
+! corresponding local mean particle velocity of that species evaluated
+! during the same instant
 !------------------------------------- 
 ! loop over all fluid cells      
       DO IJK = IJKSTART3, IJKEND3
@@ -62,32 +64,19 @@
 ! loop over all particles in ijk fluid cell            
             IF (ASSOCIATED(PIC(I,J,K)%p)) THEN
                NPG = SIZE(PIC(I,J,K)%p)
-               TEMP(:) = ZERO
-               NPG_PHASE(:) = ZERO
-
+                      
+               TEMP = ZERO
                DO LL = 1, NPG 
                   NP = PIC(I,J,K)%p(LL)
                   M = PIJK(NP,5)
-                  NPG_PHASE(M) = NPG_PHASE(M) + 1
-
-                  TEMP(M) = TEMP(M) + &
-                     (DES_VEL_NEW(NP,1)-DES_U_s(IJK,M))**2 
-                  TEMP(M) = TEMP(M) + &
-                     (DES_VEL_NEW(NP,2)-DES_V_s(IJK,M))**2
+            
+                  TEMP = TEMP + (DES_VEL_NEW(NP,1)-DES_U_s(IJK,M))**2 
+                  TEMP = TEMP + (DES_VEL_NEW(NP,2)-DES_V_s(IJK,M))**2
                   IF(DIMN.EQ.3) THEN 
-                     TEMP(M) = TEMP(M) + &
-                        (DES_VEL_NEW(NP,3)-DES_W_s(IJK,M))**2 
+                     TEMP = TEMP + (DES_VEL_NEW(NP,3)-DES_W_s(IJK,M))**2 
                   ENDIF
                ENDDO
-
-               DO M = 1,MMAX
-                  IF (NPG_PHASE(M) > 0 ) THEN
-                     DES_THETA(IJK,M) = TEMP(M)/&
-                        DBLE(DIMN*NPG_PHASE(M))
-                  ELSE
-                     DES_THETA(IJK,M) = ZERO
-                  ENDIF 
-               ENDDO
+               DES_THETA(IJK,M) = TEMP/(DBLE(DIMN) * DBLE(NPG))
             ENDIF
          ENDIF
       
@@ -137,8 +126,11 @@
       ENDDO
 
 !J.Musser changed PARTICLES TO PIS 
-      DES_VEL_AVG(:) = DES_VEL_AVG(:)/PIS       
+      DES_VEL_AVG(:) = DES_VEL_AVG(:)/DBLE(PIS)
 
+! The following quantities are primarily used for debugging/developing
+! and allow a quick check of the energy conservation in the system.
+! In their current form they are best applied to monodisperse cases. 
 ! Calculate x,y,z components of global energy & granular temperature
       GLOBAL_GRAN_ENERGY = ZERO
       GLOBAL_GRAN_TEMP  = ZERO
@@ -150,13 +142,13 @@
          GLOBAL_GRAN_ENERGY(:) = GLOBAL_GRAN_ENERGY(:) + &
             0.5d0*PMASS(LL)*(DES_VEL_NEW(LL,:)-DES_VEL_AVG(:))**2
          GLOBAL_GRAN_TEMP(:) = GLOBAL_GRAN_TEMP(:) + &
-            PMASS(LL)*(DES_VEL_NEW(LL,:)-DES_VEL_AVG(:))**2
+            (DES_VEL_NEW(LL,:)-DES_VEL_AVG(:))**2
 
          PC = PC + 1
       ENDDO
 
-      GLOBAL_GRAN_ENERGY(:) =  GLOBAL_GRAN_ENERGY(:) 
-      GLOBAL_GRAN_TEMP(:) =  GLOBAL_GRAN_TEMP(:)     
+      GLOBAL_GRAN_ENERGY(:) =  GLOBAL_GRAN_ENERGY(:)/DBLE(PIS)
+      GLOBAL_GRAN_TEMP(:) =  GLOBAL_GRAN_TEMP(:)/DBLE(PIS)
 
       RETURN
       END SUBROUTINE DES_GRANULAR_TEMPERATURE
