8bca04414b97ec42254d25ed7c7f6b130771f992
Janine Carney <janine.carney@netl.doe.gov>
2011-02-18 18:51:49 +0000

    minor correction in lees edwards check

--- model/des/check_des_data.f
+++ model/des/check_des_data.f
@@ -155,7 +155,7 @@
                CALL MFIX_EXIT(myPE)
             ENDIF
          ELSEIF(DIMN.EQ.3) THEN
-            IF(TRIM(DES_LE_SHEAR_DIR) .NE. 'DUDY' .AND. &
+            IF(TRIM(DES_LE_SHEAR_DIR) .NE. 'DUDY') THEN ! .AND. & 
 !               TRIM(DES_LE_SHEAR_DIR) .NE. 'DUDZ' .AND. &
 !               TRIM(DES_LE_SHEAR_DIR) .NE. 'DVDX' .AND. &
 !               TRIM(DES_LE_SHEAR_DIR) .NE. 'DVDZ' .AND. &
