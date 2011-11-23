ffae1bf19973c3fe8eefda0823c90de4f053279d
Rahul Garg <rahul.garg@gmail.com>
2011-11-23 20:16:07 +0000

    moving this module from examine_data.f to a seprate file so that make file dependencies work out

---
+++ post_mfix/post_precision_mod.f
@@ -0,0 +1,11 @@
+module post_precision
+
+      logical :: bPrecision = .false.
+      integer :: nPrec_location = -1
+      integer :: nPrec_time     = -1
+      integer :: nPrec_variable =  5
+      integer :: nPrec_length   =  0
+
+      character :: spec*80
+
+end module post_precision
