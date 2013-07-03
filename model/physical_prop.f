83163865fbbd4c41a6202b452866321d71f2b73d
Jordan Musser <jordan.musser@netl.doe.gov>
2013-07-03 14:47:47 +0000

    moved negative ROg messages

--- model/physical_prop.f
+++ model/physical_prop.f
@@ -142,8 +142,6 @@
 ! Maximum value for molecular weight (divided by one)
       use toleranc, only: OMW_MAX
 
-      use cutcell 
-
       implicit none
 
 ! Local Variables:
@@ -159,14 +157,19 @@
 ! Equation of State - GAS
       DOUBLE PRECISION, EXTERNAL :: EOSG
 
+! Flag to write log header
+      LOGICAL wHeader
+
       include 'function.inc'
+
+! Initialize:
+      wHeader = .TRUE.
 
 ! Average molecular weight: Xg1/Mw1 + Xg2/Mw2 + Xg3/Mw3 + ....
       IF(.NOT.database_read) call read_database0(IER)
 
       IJK_LP: DO IJK = IJKSTART3, IJKEND3 
          IF(WALL_AT(IJK)) cycle IJK_LP
-
          IF (MW_AVG == UNDEFINED) THEN 
 ! Calculating the average molecular weight of the fluid.
             MW = SUM(X_G(IJK,:NMAX(0))/MW_G(:NMAX(0)))
@@ -179,39 +182,15 @@
             RO_G(IJK) = EOSG(MW_AVG,P_G(IJK),T_G(IJK))
             ROP_G(IJK) = RO_G(IJK)*EP_G(IJK)
          ENDIF
-       
+
          IF(RO_G(IJK) < ZERO) THEN
             Err_l(myPE) = 100
-            IF(CARTESIAN_GRID) THEN 
-               WRITE(*,1001) I_OF(IJK), J_OF(IJK), K_OF(IJK), &
-                  RO_G(IJK), P_G(IJK), T_G(IJK), CUT_CELL_AT(IJK), & 
-                  SMALL_CELL_AT(IJK), xg_e(I_OF(IJK)), &
-                  yg_n(J_of(ijk)), zg_t(k_of(ijk))
-            ELSE
-               WRITE(*,1000) I_OF(IJK), J_OF(IJK), K_OF(IJK), &
-                  RO_G(IJK), P_G(IJK), T_G(IJK)
-            ENDIF
+            CALL ROgErr_LOG(IJK, wHeader)
          ENDIF
       ENDDO IJK_LP
 
+
       RETURN
-
- 1000 FORMAT(1X,'Message from: PHYSICAL_PROP',/& 
-            'WARNING: Gas density negative in this cell: ', /&
-            'I = ',I4,2X,' J = ',I4,2X,' K = ',I4, /&
-            'Values of variables: ','RO_g = ', G12.5, 2X, &
-            'P_g = ', G12.5, 2X, 'T_g = ', G12.5, /&
-            'Suggestion: Lower UR_FAC(1) in mfix.dat')
-
- 1001 FORMAT(1X,'Message from: PHYSICAL_PROP',/& 
-            'WARNING: Gas density negative in this cell: ', /&
-            'I = ',I4,2X,' J = ',I4,2X,' K = ',I4, /&
-            'Values of variables: ','RO_g = ', G12.5, 2X, &
-            'P_g = ', G12.5, 2X, 'T_g = ', G12.5, /&
-            'CUT CELL, SMALL CELL ?', 2(2x,L1), /&
-            'East, North, and Top coodinate = ', 3(2x, g17.8), / & 
-            'Suggestion: Lower UR_FAC(1) in mfix.dat')
-
       END SUBROUTINE PHYSICAL_PROP_ROg
 
 
@@ -425,5 +404,84 @@
       return
       END SUBROUTINE PHYSICAL_PROP_Dp
 
+
+
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
+!                                                                      C
+!  Subroutine: NegROg_LOG                                              C
+!  Purpose: Record information about the location and conditions that  C
+!           resulted in a negative gas phase density.                  C
+!                                                                      C
+!  Author: J. Musser                                  Date: 28-JUN-13  C
+!  Reviewer:                                          Date:            C
+!                                                                      C
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
+      SUBROUTINE ROgErr_LOG(IJK, tHeader)
+
+! Simulation time
+      use run, only: TIME
+! Gas phase temperature.
+      use fldvar, only: T_g
+! Gas phase density (compressible).
+      use fldvar, only: RO_g
+! Gas phase pressure.
+      use fldvar, only: P_g
+      use cutcell 
+
+      INTEGER, intent(in) :: IJK
+      LOGICAL, intent(inout) :: tHeader
+
+      LOGICAL :: lExists
+      CHARACTER*32 :: lFile
+      INTEGER, parameter :: lUnit = 4868
+      LOGICAL, save :: fHeader = .TRUE.
+
+
+      lFile = ''; 
+      if(numPEs > 1) then
+         write(lFile,"('ROgErr_',I4.4,'.log')") myPE
+      else
+         write(lFile,"('ROgErr.log')")
+      endif
+      inquire(file=trim(lFile),exist=lExists)
+      if(lExists) then
+         open(lUnit,file=trim(adjustl(lFile)),                         &
+            status='old', position='append')
+      else
+         open(lUnit,file=trim(adjustl(lFile)), status='new')
+      endif
+
+      if(fHeader) then
+         write(lUnit,1000)
+         fHeader = .FALSE.
+      endif
+
+      if(tHeader) then
+         write(lUnit,"(/2x,'Simulation time: ',g11.5)") TIME
+         tHeader = .FALSE.
+      endif
+
+      write(lUnit,1001) IJK, I_OF(IJK), J_OF(IJK), K_OF(IJK)
+      write(lUnit,"(6x,A,1X,g11.5,$)") 'RO_g:', RO_g(IJK)
+      write(lUnit,"(2x,A,1X,g11.5,$)") 'P_g:', P_g(IJK)
+      write(lUnit,"(2x,A,1X,g11.5)") 'T_g:', T_g(IJK)
+      if(CARTESIAN_GRID) then
+         write(lUnit,"(6x,A,1X,L1,$)") 'Cut Cell:', CUT_CELL_AT(IJK)
+         write(lUnit,"(6x,A,1X,L1)") 'Small Cell:', SMALL_CELL_AT(IJK)
+         write(lUnit,"(6x,'Coordinates (E/N/T): ',1X,3(2x, g17.8))") &
+            xg_e(I_OF(IJK)), yg_n(J_of(ijk)), zg_t(k_of(ijk))
+      endif
+
+      close(lUnit)
+
+      RETURN
+
+ 1000 FORMAT(2X,'One or more cells have reported a negative gas',      &
+         ' density (RO_g(IJK)). If',/2x,'this is a persistent issue,', &
+         ' lower UR_FAC(1) in mfix.dat.')
+
+ 1001 FORMAT(/4X,'IJK: ',I8,7X,'I: ',I4,'  J: ',I4,'  K: ',I4)
+
+      END SUBROUTINE ROgErr_LOG
 
       END SUBROUTINE PHYSICAL_PROP
