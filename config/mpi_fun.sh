d15bc491254800523cb224df289dc6fba05beac4
Jordan Musser <jordan.musser@netl.doe.gov>
2013-12-20 12:29:03 +0000

    Added new check to pick up inc file from new compiler flag. Needed for SBEUC PGI install.

--- config/mpi_fun.sh
+++ config/mpi_fun.sh
@@ -6,41 +6,62 @@
 #######################################################################
 SET_MPI_INCLUDE(){
 
-  if test ${USE_DMP} = 1; then
+# Serial runs uses the fake mpi include file.
+  if test ${USE_DMP} = 0; then
+    MPI_INCLUDE_PATH="${MFIX_SRC}/dmp_modules/mpi_donothing"
+  else
     echo "Searching for mpif.h file"
-# User supplied file include path.
-    if test ! -z ${MPI_INCLUDE_PATH}; then
-      echo "User defined" > /dev/null 2>&1;
-# User supplied mpi path.
-    elif test ! -z ${MPI_PATH}; then
+  fi
+
+# This may have been provided via input arguments. Thus, we have to
+# check that MPI_INCLUDE_PATH isn't set for all checks.
+
+  if test -z ${MPI_INCLUDE_PATH}; then
+# User supplied mpi path but not the include path. Maybe the include
+# path is a subdirectory of the mpi directory?
+    if test ! -z ${MPI_PATH}; then
       inc=${MPI_PATH}/include
-      if test -f ${inc}; then
-        MPI_INCLUDE_PATH=${inc}
-      fi
+      if test -f ${inc}; then  MPI_INCLUDE_PATH=${inc}; fi
+    fi
+  fi
 
 # We -might- be able to get it with showme: (Open MPI)
-    elif (eval ${FORTRAN_CMD} -showme:incdirs) > /dev/null 2>&1; then
+  if test -z ${MPI_INCLUDE_PATH}; then
+    if (eval ${FORTRAN_CMD} -showme:incdirs) > /dev/null 2>&1; then
       MPI_INCLUDE_PATH=$(eval ${FORTRAN_CMD} -showme:incdirs)
+    fi
+  fi
 
 # We -might- be able to get it with show (Intel MPI)
-    elif (eval ${FORTRAN_CMD} -show) > /dev/null 2>&1; then
+  if test -z ${MPI_INCLUDE_PATH}; then
+    if (eval ${FORTRAN_CMD} -show) > /dev/null 2>&1; then
       out=$(eval ${FORTRAN_CMD} -show)
       for arg in ${out}; do
         dir=$(echo ${arg} | grep include | sed -e 's/-I\(.*\)\/include/\1/')
         if test ! -z ${dir}; then
           inc=${dir}/include
-          if test -f ${inc}/mpif.h; then
-            MPI_INCLUDE_PATH=${inc}
-          fi
+          if test -f ${inc}/mpif.h; then MPI_INCLUDE_PATH=${inc}; fi
         fi
       done
     fi
-
-# Serial runs uses the fake mpi include file.
-  else
-    MPI_INCLUDE_PATH="${MFIX_SRC}/dmp_modules/mpi_donothing"
   fi
 
+# We -might- be able to get it with compile-info (MPICH)
+  if test -z ${MPI_INCLUDE_PATH}; then
+    if (eval ${FORTRAN_CMD} -compile-info) > /dev/null 2>&1; then
+      out=$(eval ${FORTRAN_CMD} -compile-info)
+      for arg in ${out}; do
+        dir=$(echo ${arg} | grep include | sed -e 's/-I\(.*\)\/include/\1/')
+        if test ! -z ${dir}; then
+          inc=${dir}/include
+          if test -f ${inc}/mpif.h; then MPI_INCLUDE_PATH=${inc}; fi
+        fi
+      done
+    fi
+  fi
+
+
+# I've checked all the placed I know where to look. Ask the user.
   if test -z ${MPI_INCLUDE_PATH}; then
     echo "  Unable to locate mpif.h"
     echo -n "  Please provide the location of the mpif.h file: "
