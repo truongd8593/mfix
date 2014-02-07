61b2f2a1406c95907c28c73ef0951f9d1af8d1fa
Jordan Musser <jordan.musser@netl.doe.gov>
2014-02-07 15:26:48 +0000

    clean up

--- model/check_data/check_gas_phase.f
+++ model/check_data/check_gas_phase.f
@@ -12,17 +12,28 @@
 
 ! Global Variables:
 !---------------------------------------------------------------------//
-      USE compar
-      USE param 
-      USE param1 
-      USE physprop
-      USE funits 
-      USE run
-      USE indices
-      USE rxns
+! Flag: Solve species equations.
+      use run, only: SPECIES_EQ
+! Flag: Use legacy reaction rates implemenation
+      use rxns, only: USE_RRATES
+! User specified: Constant gas viscosity
+      use physprop, only: MU_G0
+! User specified: Constant gas thermal conductivity
+      use physprop, only: K_G0
+! User specified: Constant gas mixture diffusion coefficient
+      use physprop, only: DIF_G0
+! User specified: Constant gas specific heat
+      use physprop, only: C_PG0
+! User specified: Constant gas density
+      use physprop, only: RO_G0
+! User specified: Constant gas mixture molecular weight
+      use physprop, only: MW_AVG
+
 
 ! Global Parameters:
 !---------------------------------------------------------------------//
+! Parameter constants
+      use param1, only: UNDEFINED, ZERO
 
 ! Use the error manager for posting error messages.
 !---------------------------------------------------------------------//
@@ -34,7 +45,164 @@
 
 ! Local Variables::
 !---------------------------------------------------------------------//
-      INTEGER :: N 
+! NONE
+
+!......................................................................!
+
+
+! Initialize the error manager.
+      CALL INIT_ERR_MSG("CHECK_GAS_PHASE")
+
+
+! CHECK MU_g0
+      IF (MU_G0 <= ZERO) THEN 
+         WRITE(ERR_MSG,1001) 'MU_G0', iVal(MU_G0)
+         CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
+      ENDIF 
+
+! CHECK K_g0
+      IF (K_G0 < ZERO) THEN 
+         WRITE(ERR_MSG,1001) 'K_G0', iVal(K_G0)
+         CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
+      ENDIF 
+
+! CHECK C_pg0
+      IF (C_PG0 < ZERO) THEN 
+         WRITE(ERR_MSG,1001) 'C_PG0', iVal(C_PG0)
+         CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
+      ENDIF 
+
+! CHECK DIF_g0
+      IF (DIF_G0 < ZERO) THEN
+         WRITE(ERR_MSG,1001) 'DIF_g0', iVal(DIF_g0)
+         CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
+      ENDIF 
+
+! Check the input specifications for gas species.
+      IF(USE_RRATES)THEN
+         CALL CHECK_GAS_SPECIES_LEGACY
+      ELSE
+         CALL CHECK_GAS_SPECIES
+      ENDIF
+
+! CHECK MW_AVG
+      IF (SPECIES_EQ(0)) THEN
+! MW_AVG is defined and the gas phase species equations are solved, then
+! the user specified average molecular weight is ignored. The gas phase
+! mixture molecular weight (MW_MIX_g) is used instead.
+         IF (MW_AVG /= UNDEFINED) THEN 
+            WRITE (ERR_MSG, 1100) 'solving species equations'
+            CALL FLUSH_ERR_MSG
+            MW_AVG = UNDEFINED 
+         ENDIF 
+      ELSE 
+! When the species equations are not solved and the gas phase is
+! compressible, verify that the user provided average molecular weight
+! has a physical value. (This does not include the case where MW_AVG
+! is UNDEFINED.)
+         IF (RO_G0 == UNDEFINED) THEN 
+            IF (MW_AVG <= ZERO) THEN 
+               WRITE(ERR_MSG, 1001) 'MW_AVG', iVal(MW_AVG)
+               CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
+            ENDIF 
+         ELSE 
+! Gas density for incompressible flows must be positive.
+            IF (RO_G0 < ZERO) THEN
+               WRITE(ERR_MSG, 1001) 'RO_G0', iVal(RO_G0)
+               CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
+            ENDIF
+! Incompressible simulations do not need MW_AVG. Notify the user that 
+! the provided data is ignored.
+            IF (MW_AVG /= UNDEFINED)THEN
+               WRITE(ERR_MSG, 1100) 'RO_g0 is specified'
+               CALL FLUSH_ERR_MSG
+            ENDIF
+
+         ENDIF 
+      ENDIF
+
+
+! Finalize the error manager
+      CALL FINL_ERR_MSG
+
+
+      RETURN  
+
+ 1000 FORMAT('Error 1000: Required input not specified: ',A,/'Please ',&
+            'correct the mfix.dat file.')
+
+ 1001 FORMAT('Error 1001: Illegal or unknown input: ',A,' = ',A,/      &
+         'Please correct the mfix.dat file.')
+
+
+ 1100 FORMAT('Message 2000: MW_AVG is not needed when ',A,'.')
+
+      END SUBROUTINE CHECK_GAS_PHASE
+
+
+
+!----------------------------------------------------------------------!
+! Subroutine: CHECK_GAS_SPECIES                                        !
+! Purpose: Gas phase species checks.                                   !
+!                                                                      !
+! Author: J. Musser                                  Date: 07-FEB-14   !
+!----------------------------------------------------------------------!
+      SUBROUTINE CHECK_GAS_SPECIES
+
+!      USE compar
+!      USE funits 
+!      USE param 
+!      USE param1 
+!      use physprop
+!      use rxns
+ !     USE RUN
+
+
+! Global Variables:
+!---------------------------------------------------------------------//
+! Flag: Solve energy equations
+      use run, only: ENERGY_EQ
+! Flag: Solve species equations
+      use run, only: SPECIES_EQ
+! Flag: Database for phase X was read for species Y
+      use rxns, only: rDatabase
+! Flag: Database was read. (legacy)
+      use physprop, only: DATABASE_READ
+! Gas phase species database names.
+      use rxns, only: SPECIES_g
+! Gas phase molecular weights.
+      use physprop, only: MW_g
+! Number of gas phase species.
+      use physprop, only: NMAX, NMAX_g
+! User specified: Constant gas phase specific heat
+      use physprop, only: C_PG0
+! User specified: Constant gas density
+      use physprop, only: RO_G0
+! User specified: Constant gas phase mixture molecular weight
+      use physprop, only: MW_AVG
+
+
+! Global Parameters:
+!---------------------------------------------------------------------//
+! Maximum number of gas phase species.
+      USE param, only: DIM_N_g
+! Constants.
+      USE param1, only: UNDEFINED, UNDEFINED_I, UNDEFINED_C
+      USE param1, only: ZERO
+
+
+! Use the error manager for posting error messages.
+!---------------------------------------------------------------------//
+      use error_manager
+
+
+      implicit none
+
+
+! Local Variables:
+!---------------------------------------------------------------------//
+! Loop counter.
+      INTEGER :: N
 
 ! Flag that the energy equations are solved and constant gas phase
 ! specific heat is undefined.
@@ -51,205 +219,11 @@
 ! If true, a call to the thermochemical database is made.
       LOGICAL SEQ_MWg
 
-! Flag that the user was already warned why a call to the thermo-
-! chemical database is being made.
-      LOGICAL WARNED_USR
-
-! Flag indicating that the thermochemical database header was output 
-! to the screen. (Miminize messages)
-      LOGICAL thermoHeader
-
 
 !......................................................................!
 
 
 ! Initialize the error manager.
-      CALL INIT_ERR_MSG("CHECK_GAS_PHASE")
-
-
-! CHECK MU_g0
-      IF (MU_G0 <= ZERO) THEN 
-         WRITE(ERR_MSG,1002) 'MU_G0', MU_G0
-         CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
-      ENDIF 
-
-! CHECK K_g0
-      IF (K_G0 < ZERO) THEN 
-         WRITE(ERR_MSG,1002) 'K_G0', K_G0
-         CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
-      ENDIF 
-
-! CHECK C_pg0
-      IF (C_PG0 < ZERO) THEN 
-         WRITE(ERR_MSG,1002) 'C_PG0', C_PG0
-         CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
-      ENDIF 
-
-! CHECK DIF_g0
-      IF (DIF_G0 < ZERO) THEN
-         WRITE(ERR_MSG,1002) 'DIF_g0', DIF_g0
-         CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
-      ENDIF 
-
-
-
-
- 1000 FORMAT('Error 1000: Required input not specified: ',A,/'Please ',&
-            'correct the mfix.dat file.')
-
- 1001 FORMAT('Error 1001: Illegal or unknown input: ',A,' = ',A,/      &
-         'Please correct the mfix.dat file.')
-
- 1002 FORMAT('Error 1002: Illegal or unknown input: ',A,' = ',E14.6,/  &
-         'Please correct the mfix.dat file.')
-
- 1003 FORMAT('Error 1003: Illegal or unknown input: ',A,' = ',I4,/     &
-         'Please correct the mfix.dat file.')
-
-
-
-! Check the input specifications for gas species.
-      IF(USE_RRATES)THEN
-         CALL CHECK_GAS_SPECIES_LEGACY
-      ELSE
-         CALL CHECK_GAS_SPECIES
-      ENDIF
-
-
-
-
-! CHECK MW_AVG
-      IF (SPECIES_EQ(0)) THEN
-! MW_AVG is defined and the gas phase species equations are solved, then
-! the user specified average molecular weight is ignored. The gas phase
-! mixture molecular weight (MW_MIX_g) is used instead.
-         IF (MW_AVG /= UNDEFINED) THEN 
-            WRITE (ERR_MSG, 1410)
-            CALL FLUSH_ERR_MSG
-            MW_AVG = UNDEFINED 
-         ENDIF 
-
- 1410 FORMAT('Message: MW_AVG not needed when solving species equations')
-
-      ELSE 
-! When the species equations are not solved and the gas phase is
-! compressible, verify that the user provided average molecular weight
-! has a physical value. (This does not include the case where MW_AVG
-! is UNDEFINED.)
-         IF (RO_G0 == UNDEFINED) THEN 
-            IF (MW_AVG <= ZERO) THEN 
-               WRITE(ERR_MSG, 1002) 'MW_AVG', MW_AVG
-               CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
-            ENDIF 
-         ELSE 
-! Gas density for incompressible flows must be positive.
-            IF (RO_G0 < ZERO) THEN
-               WRITE(ERR_MSG, 1002) 'RO_G0', RO_G0
-               CALL FLUSH_ERR_MSG(ABORT=.TRUE.)
-            ENDIF
-! Incompressible simulations do not need MW_AVG. Notify the user that 
-! the provided data is ignored.
-            IF (MW_AVG /= UNDEFINED)THEN
-               WRITE(ERR_MSG, 1400)
-               CALL FLUSH_ERR_MSG
-            ENDIF
-
- 1400 FORMAT('Message: MW_AVG not needed when RO_g0 is specified.')
-
-         ENDIF 
-      ENDIF
-
-
-
-
-! Finalize the error manager
-      CALL FINL_ERR_MSG
-
-
-      RETURN  
-
-
- 1050 FORMAT('Message: NMAX(0) is specied for the gas phase. This is', &
-         ' a legacy',/'variable and NMAX_g should be used. Copying',   &
-         ' NMAX(0) to NMAX_g.')
-
- 1051 FORMAT('Message: NMAX_g and NMAX(0) are both given for the gas', &
-         ' phase and do',/' not match. NMAX(0) is a legacy variable',  &
-         ' and is not required.',/' Please correct the data file.')
-
- 1052 FORMAT(' Message: The number of gas speices (NMAX_g) is not',    &
-         ' specified. Please',/' correct the data file.')
-
- 1053 FORMAT('Message: The number of gas species (NMAX_g) is too ',    &
-         'large. Please',/' correct the data file.')
-
-
-
- 1057 FORMAT('Message: Gas phase species ',I2,' name (SPECIES_g) is',  &
-         ' undefined.',/'Please correct the data file.')
-
-
-
- 1500 FORMAT(1X,/,1X,'MW_g for gas species ',I3,' not specified') 
- 1501 FORMAT(1X,/,1X,'MW_g for gas species ',I3,' specified') 
-
-      END SUBROUTINE CHECK_GAS_PHASE
-
-
-
-!----------------------------------------------------------------------!
-! Subroutine: CHECK_GAS_SPECIES                                     !
-! Purpose: Clear solids phase species data from continuum variables    !
-! and notify the user.                                                 !
-!                                                                      !
-! Author: J. Musser                                  Date: 02-NOV-12   !
-! Reviewer:                                          Date:             !
-!                                                                      !
-! Literature/Document References: MFiX Readme                          !
-!                                                                      !
-! Variables modified: NMAX(1:DIM_M), NMAX_s(:), SPECIES_S(:,:),        !
-!                     SPECIES_ALIAS_S(:,:)                             !
-!                                                                      !
-!----------------------------------------------------------------------!
-      SUBROUTINE CHECK_GAS_SPECIES
-
-      USE compar
-      USE funits 
-      USE param 
-      USE param1 
-      USE physprop
-      USE rxns
-      USE RUN
-
-      use error_manager
-
-      IMPLICIT NONE
-
-! Total number of solids phases
-      INTEGER  :: MMAX_LL
-
-
-! Flag that the user was already warned why a call to the thermo-
-! chemical database is being made.
-      LOGICAL WARNED_USR
-
-! Flag that the energy equations are solved and specified solids phase
-! specific heat is undefined.
-! If true, a call to the thermochemical database is made.
-      LOGICAL EEQ_CPG
-
-! Flag that the solids phase species equations are solved and the 
-! molecular weight for a species are not given in the data file.
-! If true, a call to the thermochemical database is made.
-      LOGICAL SEQ_MWg
-
-      LOGICAL MWg_ROg
-
-!      LOGICAL thermoHeader
-
-      INTEGER :: N
-
-
       CALL INIT_ERR_MSG("CHECK_GAS_SPECIES")
 
 
@@ -344,7 +318,7 @@
 ! Update the log files.
             WRITE(ERR_MSG, 3001) N, trim(SPECIES_g(N))
             CALL FLUSH_ERR_MSG(HEADER=.FALSE., FOOTER=.FALSE.)
-            3001 FORMAT(2x,'>',I3,': Species: ',A)
+            3001 FORMAT(/2x,'>',I3,': Species: ',A)
 ! Read the database.
             CALL READ_DATABASE('TFM', 0, N, SPECIES_g(N), MW_g(N))
 ! Flag variable to stating that the database was read.
@@ -352,7 +326,7 @@
 ! Flag the legacy variable to prevent re-reading the database.
             DATABASE_READ = .TRUE.
          ENDDO ! Loop over species
-
+         CALL FLUSH_ERR_MSG(HEADER=.FALSE.)
       ENDIF
 
 ! Verify that no additional species information was given.
