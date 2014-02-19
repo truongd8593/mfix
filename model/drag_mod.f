38d60aeb44ad1bd3291c4d44e07ec5a68e089743
Jordan Musser <jordan.musser@netl.doe.gov>
2014-02-19 19:39:11 +0000

    changed Cd function enums to be more descriptive

--- model/drag_mod.f
+++ model/drag_mod.f
@@ -28,17 +28,16 @@
 ! Temporary storage: Volume x average at momentum cell centers
 !      DOUBLE PRECISION, DIMENSION(:, :), ALLOCATABLE ::  VxF_gs
  
-
 ! Number specifying the C_Ds function to use.
-      INTEGER :: FUN_C_DS_ENUM
+      INTEGER :: CD_FUNCTION_ENUM
 
 ! Enumerated list of C_Ds functions for calculating either the
 ! 1) Single sphere drag correlation 
 ! 2) Single sphere drag correlation multipling Reynolds number or
-      INTEGER, PARAMETER :: C_DS_SN    = 1  ! Schiller and Naumann (1933)
-      INTEGER, PARAMETER :: C_DSXRE_DV = 2  ! Dalla Valle (1948)
-      INTEGER, PARAMETER :: C_DS_DEL   = 3  ! Dellino et al. (2005)
-      INTEGER, PARAMETER :: C_DSxRE_TL = 4  ! Turton and Levenspiel (1986)
+      INTEGER, PARAMETER :: SCHILLER_1933 = 1  ! Schiller and Naumann (1933)
+      INTEGER, PARAMETER :: DALLA_1948    = 2  ! Dalla Valle (1948)
+      INTEGER, PARAMETER :: DELLINO_2005  = 3  ! Dellino et al. (2005)
+      INTEGER, PARAMETER :: TURTON_1986   = 4  ! Turton and Levenspiel (1986)
 
 !!!HPF$ align F_gs(:, *) with TT(:)
 !!!HPF$ align F_ss(:, *) with TT(:)
