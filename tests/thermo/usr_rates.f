475cb22290952f3e5622e535e8806749521069a7
Jordan Musser <jordan.musser@netl.doe.gov>
2014-03-29 12:01:07 +0000

    added comments to usr_rates to explain the 49

--- tests/thermo/usr_rates.f
+++ tests/thermo/usr_rates.f
@@ -53,6 +53,14 @@
 !`````````````````````````````````````````````````````````````````````//
       INTEGER :: L
 
+! This case is designed so that there is one fluid cell for each
+! reaction. The first fluid cell start with and IJK value of 50.
+! Therefore, subtract of 49 to have a one-to-one map between the
+! rates array and fluid cells.
+!
+! This is overly complicated but done so that the table generated in
+! usr3 for this test case can be populated without directly modifying
+! any real source code. 
       L = IJK - 49
       RATES(L) = ONE
 
