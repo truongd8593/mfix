75e23a9bcd1d160281787c6a354129240cda4fbd
Jordan Musser <jordan.musser@netl.doe.gov>
2014-05-20 08:12:58 -0400

    Modified zero-solids values.
    The correction of transpiration was modified to only use GAMA_GS
    when its value is greater than 'small_number.'  Using ZERO in the
    logical condition results in floating point exceptions when GAMA_GS
    is near-zero.
    
    This issue was identified by the failure of the Spouted Bed
    Combustor tutorial.

--- model/calc_gama.f
+++ model/calc_gama.f
@@ -1,55 +1,26 @@
-!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
-!                                                                      C
-!  Module name: CALC_GAMA(HEAT_TR, IER)                               C
-!  Purpose: Calculate gas-solids heat transfer coefficients            C
-!                                                                      C
-!  Author: M. Syamlal                                 Date: 15-JUL-92  C
-!  Reviewer:                                          Date: dd-mmm-yy  C
-!                                                                      C
-!  Revision Number: 1                                                  C
-!  Purpose: Modifications for variable grid size capability,           C
-!           logic for volume-weighted averaging                        C
-!  Author: W. Rogers                                  Date: 20-JUL-92  C
-!  Reviewer: P. Nicoletti                             Date: 11-DEC-92  C
-!  Revision Number: 2                                                  C
-!  Purpose: Correct the heat transfer coefficient for transpiration    C
-!           effect                                                     C
-!  Author: M. Syamlal                                 Date: 25-AUG-93  C
-!                                                                      C
-!  Revision Number: 3                                                  C
-!  Purpose: MFIX 2.0 mods                                              C
-!  Author: M. Syamlal                                 Date: 25-APR-96  C
-!  Literature/Document References:                                     C
-!    Gunn, D.J.,"Transfer of Heat or Mass to Particles in Fixed and    C
-!      Fluidized Beds," Int. J. Heat Mass Transf., 21, 467(1978)       C
-!    Bird, R.B., W.E. Stewart, E.N. Lightfoot, Transport Phenomena,    C
-!      John Wiley & Sons, New York, 1960.                              C
-!                                                                      C
-!  Variables referenced:                                               C
-!  Variables modified: GAMA_gs                             C
-!                                                                      C
-!  Local variables:                                                    C
-!                                                                      C
-!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
-!
-      SUBROUTINE CALC_GAMA(HEAT_TR, IER) 
-!...Translated by Pacific-Sierra Research VAST-90 2.06G5  12:17:31  12/09/98  
-!...Switches: -xf
-!
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv!
+!                                                                      !
+!  Subroutine: CALC_GAMA                                               !
+!  Author: M. Syamlal                                 Date: 15-JUL-92  !
+!                                                                      !
+!  Purpose: Calculate gas-solids heat transfer coefficients            !
+!                                                                      !
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^!
+      SUBROUTINE CALC_GAMA(HEAT_TR, IER)
 !
 !-----------------------------------------------
 !   M o d u l e s 
 !-----------------------------------------------
-      USE param 
-      USE param1 
-      USE parallel 
+      USE param
+      USE param1
+      USE parallel
       USE physprop
       USE geometry
       USE fldvar
       USE energy
       USE rxns
       USE indices
-      USE compar    
+      USE compar
       USE sendrecv
       USE discretelement
 
@@ -107,66 +78,62 @@
       IF (DISCRETE_ELEMENT) RETURN
 
 ! this needs to be generalized
-      DO M = 1, SMAX 
-         IF (HEAT_TR(0,M)) THEN 
+      DO M = 1, SMAX
+         IF (HEAT_TR(0,M)) THEN
 
-!!!!$omp  parallel do &
-!!!!$omp& private(I, IJK, M, EP_g2, Pr1o3, UGC, VGC, WGC, USCM, VSCM, &
-!!!!$omp&  WSCM, VREL, Re, LM, FAC ) &
-!!!!$omp& schedule(dynamic,chunk_size)
             DO IJK = ijkstart3, ijkend3
- 
-               IF (FLUIDorP_FLOW_AT(IJK)) THEN 
-                  I = I_OF(IJK) 
-                  EP_G2 = EP_G(IJK)*EP_G(IJK) 
+
+               IF (FLUIDorP_FLOW_AT(IJK)) THEN
+                  I = I_OF(IJK)
+                  EP_G2 = EP_G(IJK)*EP_G(IJK)
 
 ! Calculate Prandtl number to the 1/3 power
                   if(K_G(IJK) > ZERO) then
-                    PR1O3 = (C_PG(IJK)*MU_G(IJK)/K_G(IJK))**(1.D0/3.D0) 
+                    PR1O3 = (C_PG(IJK)*MU_G(IJK)/K_G(IJK))**(1.D0/3.D0)
                   else
-                    PR1O3 = LARGE_NUMBER 
+                    PR1O3 = LARGE_NUMBER
                   endif
 
 ! Calculate velocity components at the cell center for gas phase
-                  UGC = HALF*(U_G(IJK)+U_G(IM_OF(IJK))) 
-                  VGC = HALF*(V_G(IJK)+V_G(JM_OF(IJK))) 
-                  WGC = HALF*(W_G(IJK)+W_G(KM_OF(IJK))) 
+                  UGC = HALF*(U_G(IJK)+U_G(IM_OF(IJK)))
+                  VGC = HALF*(V_G(IJK)+V_G(JM_OF(IJK)))
+                  WGC = HALF*(W_G(IJK)+W_G(KM_OF(IJK)))
 
 ! Calculate velocity components at the cell center for solids phase m
-                  USCM = HALF*(U_S(IJK,M)+U_S(IM_OF(IJK),M)) 
-                  VSCM = HALF*(V_S(IJK,M)+V_S(JM_OF(IJK),M)) 
-                  WSCM = HALF*(W_S(IJK,M)+W_S(KM_OF(IJK),M)) 
+                  USCM = HALF*(U_S(IJK,M)+U_S(IM_OF(IJK),M))
+                  VSCM = HALF*(V_S(IJK,M)+V_S(JM_OF(IJK),M))
+                  WSCM = HALF*(W_S(IJK,M)+W_S(KM_OF(IJK),M))
 
 ! Calculate the magnitude of gas-solids relative velocity
-                  VREL=SQRT((UGC-USCM)**2+(VGC-VSCM)**2+(WGC-WSCM)**2) 
-  
+                  VREL=SQRT((UGC-USCM)**2+(VGC-VSCM)**2+(WGC-WSCM)**2)
+
                   if(MU_G(IJK) > ZERO)then
                      RE = EP_G(IJK)*D_P(IJK,M)*VREL*RO_G(IJK)/MU_G(IJK)
-                  else 
+                  else
                      RE = LARGE_NUMBER
                   endif
 
 ! Calculate gas-solids heat transfer coefficient (Gunn 1978)
                   GAMA_GS(IJK,M) = ((7.D0 - 10.D0*EP_G(IJK)+5.D0*EP_G2)*(ONE+0.7D0*RE**&
                      0.2D0*PR1O3)+(1.33D0 - 2.4D0*EP_G(IJK)+1.2D0*EP_G2)*RE**0.7D0*PR1O3)*(&
-                     K_G(IJK)/D_P(IJK,M))*(6.D0*EP_S(IJK,M)/D_P(IJK,M)) 
+                     K_G(IJK)/D_P(IJK,M))*(6.D0*EP_S(IJK,M)/D_P(IJK,M))
 
 ! Correct the heat transfer coefficient for transpiration
 ! Bird, Stewart, and Lightfoot (1960, p.663)
-                  IF (GAMA_GS(IJK,M) > ZERO) THEN 
-                     LM = 1 
-                     FAC = R_PHASE(IJK,LM)*C_PG(IJK)/GAMA_GS(IJK,M) 
-                     IF (ABS(FAC) < 0.1D0) THEN 
-                        GAMA_GS(IJK,M)=GAMA_GS(IJK,M)/(ONE+FAC/2.D0+FAC*FAC/6.D0) 
-                     ELSE 
-                        IF (R_PHASE(IJK,LM) > ZERO) THEN 
+                  IF (GAMA_GS(IJK,M) > SMALL_NUMBER) THEN
+                     LM = 1
+                     FAC = R_PHASE(IJK,LM)*C_PG(IJK)/GAMA_GS(IJK,M)
+                     IF (ABS(FAC) < 0.1D0) THEN
+                        GAMA_GS(IJK,M)=GAMA_GS(IJK,M)/(ONE+FAC/2.D0+FAC*FAC/6.D0)
+                     ELSE
+                        IF (R_PHASE(IJK,LM) > ZERO) THEN
                            GAMA_GS(IJK,M) = R_PHASE(IJK,LM)*C_PG(IJK)*EXP((-FAC&
-                              ))/(ONE - EXP((-FAC))) 
-                        ELSE 
+                              ))/(ONE - EXP((-FAC)))
+                        ELSE
                            GAMA_GS(IJK,M) = R_PHASE(IJK,LM)*C_PG(IJK)/(EXP(FAC)&
-                               - ONE) 
-                        ENDIF 
-                     ENDIF 
+                               - ONE)
+                        ENDIF
+                     ENDIF
                   ENDIF   ! end if gama_gs(ijk,M) > 0
 
                ENDIF   ! end if (fluidorp_flow_at(ijk) 
@@ -176,8 +143,5 @@
 
       call send_recv(GAMA_GS,2)
 
-      RETURN  
-      END SUBROUTINE CALC_GAMA 
-
-
-
+      RETURN
+      END SUBROUTINE CALC_GAMA
