2f7202164e197b64acb321fe84cb20b0e32c5f61
Jordan Musser <jordan.musser@netl.doe.gov>
2014-09-03 09:58:13 -0400

    Added notice and disclaimer

--- model/mfix.f
+++ model/mfix.f
@@ -1,44 +1,64 @@
-!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
-!                                                                      C
-!  Subroutine: MFIX                                                    C
-!  Purpose: The main module in the MFIX program                        C
-!                                                                      C
-!  Author: M. Syamlal                                 Date: 29-JAN-92  C
-!  Reviewer: P. Nicoletti, W. Rogers, S. Venkatesan   Date: 29-JAN-92  C
-!                                                                      C
-!  Revision Number: 1                                                  C
-!  Purpose: Add version                                                C
-!  Author: P.Nicoletti                                Date: 07-FEB-92  C
-!  Revision Number: 2                                                  C
-!  Purpose: Add a routine to set velocity components normal to a wall  C
-!           to zero                                                    C
-!  Author: M. Syamlal                                 Date: 14-MAY-92  C
-!  Reviewer: W. Rogers                                Date: 11-DEC-92  C
-!                                                                      C
-!  Revision Number: 3                                                  C
-!  Purpose:  Modifications for initializing & finalizing MPI through   C
-!            parallel_mpi module developed by ORNL and also starts     C
-!            the MPI Timer (MPI_WTIME)                                 C
-!                                                                      C
-!  Author:   Aeolus Res. Inc.                         Date: 01-AUG-99  C
-!  Reviewer:                                          Date: dd-mmm-yy  C
-!                                                                      C
-!  Revision Number: 4                                                  C
-!  Purpose: Modifications to call Cartesian grid subroutines           C
-!           and dashboard                                              C
-!                                                                      C
-!  Author:   Jeff Dietiker                            Date: 01-JUL-09  C
-!  Reviewer:                                          Date: dd-mmm-yy  C
-!                                                                      C
-!  Literature/Document References:                                     C
-!                                                                      C
-!  Variables referenced: RUN_TYPE                                      C
-!  Variables modified: CPU0, ID_VERSION                                C
-!                                                                      C
-!  Local variables:       CPU1, CPUTIME_USED,L                         C
-!                                                                      C
-!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
-
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv!
+!                                                                      !
+!  Subroutine: MFIX                                                    !
+!  Author: M. Syamlal                                 Date: 29-JAN-92  !
+!                                                                      !
+!  Purpose: The main module in the MFIX program                        !
+!                                                                      !
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^!
+!
+!> \mainpage Multiphase Flow with Interphase eXchanges
+!!
+!! MFIX is a general-purpose computer code developed at the National
+!! Energy Technology Laboratory, NETL, for describing the hydrodynamics,
+!! heat transfer, and chemical reactions in fluid-solid systems.
+!!
+!! It has been used for describing bubbling and circulating fluidized 
+!! beds and spouted beds. MFiX calculations give transient data on the 
+!! three-dimensional distribution of pressure, velocity, temperature,
+!! and species mass fractions. MFiX code is based on a generally
+!! accepted set of multiphase flow equations. The code is used as a 
+!! "test-stand" for testing and developing multiphase flow constitutive
+!!  equations. 
+!!
+!! \section Notice
+!! Neither the United States Government nor any agency thereof, nor any
+!! of their employees, makes any warranty, expressed or implied, or
+!! assumes any legal liability or responsibility for the accuracy,
+!! completeness, or usefulness of any information, apparatus, product,
+!! or process disclosed or represents that its use would not infringe
+!! privately owned rights.
+!!
+!! * MFIX is provided without any user support for applications in the
+!!   user's immediate organization. It should not be redistributed in 
+!!   whole or in part.
+!!
+!! * The use of MFIX is to be acknowledged in any published paper based
+!!   on computations using this software by citing the MFIX theory
+!!   manual. Some of the submodels are being developed by researchers
+!!   outside of NETL. The use of such submodels is to be acknowledged
+!!   by citing the appropriate papers of the developers of the submodels.
+!!
+!! * The authors would appreciate receiving any reports of bugs or other
+!!   difficulties with the software, enhancements to the software, and
+!!   accounts of practical applications of this software.
+!!
+!! \section Disclaimer
+!! This report was prepared as an account of work sponsored by an agency 
+!! of the United States Government. Neither the United States Government 
+!! nor any agency thereof, nor any of their employees, makes any 
+!! warranty, express or implied, or assumes any legal liability or 
+!! responsibility for the accuracy, completeness, or usefulness of any
+!! information, apparatus, product, or process disclosed, or represents 
+!! that its use would not infringe privately owned rights. Reference
+!! herein to any specific commercial product, process, or service by
+!! trade name, trademark, manufacturer, or otherwise does not
+!! necessarily constitute or imply its endorsement, recommendation, or 
+!! favoring by the United States Government or any agency thereof. The 
+!! views and opinions of authors expressed herein do not necessarily 
+!! state or reflect those of the United States Government or any 
+!! agency thereof.
+                           
       PROGRAM MFIX
 
 !-----------------------------------------------
