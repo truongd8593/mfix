bb24476e6c6479062e645592dd3c567301abf2f2
Surya Deb <surya.deb@contr.netl.doe.gov>
2015-01-13 11:27:54 -0500

    inline_pack_mod.f         Changed the name of the file
         	inline_unpack_mod.f
            Changed the name of the file

---
+++ model/des/inline_pack_mod.f
@@ -0,0 +1,48 @@
+!**********************************************************************!
+!                                                                      !
+!**********************************************************************!
+      module data_pack
+
+      use desmpi
+
+      interface pack_dbuf
+         module procedure pack_db0, pack_db1
+      end interface pack_dbuf
+
+      contains
+
+
+!----------------------------------------------------------------------!
+!                                                                      !
+!----------------------------------------------------------------------!
+      subroutine pack_db0(lbuf,idata,pface)
+      integer, intent(inout) :: lbuf
+      integer, intent(in) :: pface
+      double precision, intent(in) :: idata
+
+      dsendbuf(lbuf,pface) = idata
+      lbuf = lbuf + 1
+
+      return
+      end subroutine pack_db0
+
+!----------------------------------------------------------------------!
+!                                                                      !
+!----------------------------------------------------------------------!
+      subroutine pack_db1(lbuf,idata,pface)
+      integer, intent(inout) :: lbuf
+      integer, intent(in) :: pface
+      double precision, intent(in) :: idata(:)
+
+      integer :: lsize
+
+      lsize = size(idata)
+
+      dsendbuf(lbuf:lbuf+lsize-1,pface) = idata
+      lbuf = lbuf + lsize
+
+      return
+      end subroutine pack_db1
+
+      end module data_pack
+
---
+++ model/des/inline_unpack_mod.f
@@ -0,0 +1,48 @@
+!**********************************************************************!
+!                                                                      !
+!**********************************************************************!
+      module data_unpack
+
+      use desmpi
+
+      interface unpack_dbuf
+         module procedure unpack_db0, unpack_db1
+      end interface unpack_dbuf
+
+      contains
+
+
+!----------------------------------------------------------------------!
+!                                                                      !
+!----------------------------------------------------------------------!
+      subroutine unpack_db0(lbuf,idata,pface)
+      integer, intent(inout) :: lbuf
+      integer, intent(in) :: pface
+      double precision, intent(inout) :: idata
+
+      idata = drecvbuf(lbuf,pface) 
+      lbuf = lbuf + 1
+
+      return
+      end subroutine unpack_db0
+
+!----------------------------------------------------------------------!
+!                                                                      !
+!----------------------------------------------------------------------!
+      subroutine unpack_db1(lbuf,idata,pface)
+      integer, intent(inout) :: lbuf
+      integer, intent(in) :: pface
+      double precision, intent(inout) :: idata(:)
+
+      integer :: lsize
+
+      lsize = size(idata)
+
+      idata = drecvbuf(lbuf:lbuf+lsize-1,pface) 
+      lbuf = lbuf + lsize
+
+      return
+      end subroutine unpack_db1
+
+      end module data_unpack
+
