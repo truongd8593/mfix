b84311e141cd9a9e6131b72726ed8475b2cf70e9
Aniruddha Choudhary <aniruddha.choudhary@contr.netl.doe.gov>
2015-01-21 13:44:16 -0500

    Added newunit() function inside FUNITS module.

--- model/funits_mod.f
+++ model/funits_mod.f
@@ -26,4 +26,50 @@
 ! RUN_NAME.SPx file unit offset number
       INTEGER, PARAMETER :: UNIT_SPX = 60
 
+      CONTAINS
+
+
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv!
+!                                                                      !
+!  Module name: NEWUNIT                                                !
+!  Purpose: Finds an open i/o unit number; Usage:                      !
+!   integer myunit                                                     !
+!   open(unit=newunit(myunit),file='filename')                         ! 
+!                                                                      !
+!  Author: A. Choudhary                               Date: 01/21/2015 !
+!                                                                      !
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^!
+      INTEGER FUNCTION newunit(unit)
+      IMPLICIT NONE
+
+! optional variable to hold unit number
+      INTEGER, INTENT(OUT), OPTIONAL  :: unit
+
+! lower and upper limits to search for available units
+      INTEGER, PARAMETER  :: lun_min = 100, lun_max= 999
+
+! check to see if the unit is open      
+      LOGICAL             :: is_open
+
+! looping variable      
+      INTEGER             :: lun
+
+
+      newunit = -1
+
+      DO lun = lun_min, lun_max
+        INQUIRE(UNIT=lun, OPENED=is_open)
+        IF(.NOT.is_open) THEN
+          newunit = lun
+          EXIT
+        END IF
+      END DO
+
+      IF(present(unit)) unit=newunit
+      
+      RETURN
+      
+      END FUNCTION NEWUNIT
+
+
       END MODULE FUNITS
