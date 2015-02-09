ccef210f7bbbfc1e93ff08b966c06e40f206a673
Mark Meredith <mark.meredith@contr.netl.doe.gov>
2015-02-09 16:55:51 -0500

    fix issue #32: refactor *_GROW subroutines

--- model/des/des_allocate_mod.f
+++ model/des/des_allocate_mod.f
@@ -457,65 +457,18 @@ CONTAINS
 
         IMPLICIT NONE
 
-        call integer_grow(pairs,pair_max)
-        call integer_grow(pairs_old,pair_max)
+        call integer_grow2(pairs,pair_max)
+        call integer_grow2(pairs_old,pair_max)
 
         call logical_grow(pv_pair,pair_max)
         call logical_grow(pv_pair_old,pair_max)
 
-        call real_grow(pft_pair,pair_max)
-        call real_grow(pft_pair_old,pair_max)
-        call real_grow(pfn_pair,pair_max)
-        call real_grow(pfn_pair_old,pair_max)
+        call real_grow2(pft_pair,pair_max)
+        call real_grow2(pft_pair_old,pair_max)
+        call real_grow2(pfn_pair,pair_max)
+        call real_grow2(pfn_pair_old,pair_max)
 
         RETURN
-
-      contains
-
-        SUBROUTINE INTEGER_GROW(integer_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          INTEGER, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: integer_array
-          INTEGER, DIMENSION(:,:), ALLOCATABLE :: integer_tmp
-          INTEGER lSIZE2
-
-          lSIZE2 = size(integer_array,2)
-          allocate(integer_tmp(2,new_size))
-          integer_tmp(:,1:lSIZE2) = integer_array(:,1:lSIZE2)
-          call move_alloc(integer_tmp,integer_array)
-
-        END SUBROUTINE INTEGER_GROW
-
-        SUBROUTINE LOGICAL_GROW(logical_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          LOGICAL, DIMENSION(:), ALLOCATABLE, INTENT(INOUT) :: logical_array
-          LOGICAL, DIMENSION(:), ALLOCATABLE :: logical_tmp
-          INTEGER lSIZE2
-
-          lSIZE2 = size(logical_array,1)
-          allocate(logical_tmp(new_size))
-          logical_tmp(1:lSIZE2) = logical_array(1:lSIZE2)
-          call move_alloc(logical_tmp,logical_array)
-
-        END SUBROUTINE LOGICAL_GROW
-
-        SUBROUTINE REAL_GROW(real_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: real_array
-          DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: real_tmp
-          INTEGER lSIZE2
-
-          lSIZE2 = size(real_array,2)
-          allocate(real_tmp(3,new_size))
-          real_tmp(:,1:lSIZE2) = real_array(:,1:lSIZE2)
-          call move_alloc(real_tmp,real_array)
-
-        END SUBROUTINE REAL_GROW
 
       END SUBROUTINE PAIR_GROW
 
@@ -615,150 +568,148 @@ CONTAINS
 
       RETURN
 
-      contains
-
-        SUBROUTINE INTEGER_GROW(integer_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          INTEGER, DIMENSION(:), ALLOCATABLE, INTENT(INOUT) :: integer_array
-          INTEGER, DIMENSION(:), ALLOCATABLE :: integer_tmp
-          INTEGER lSIZE
-
-          lSIZE = size(integer_array,1)
-          allocate(integer_tmp(new_size))
-          integer_tmp(1:lSIZE) = integer_array(1:lSIZE)
-          call move_alloc(integer_tmp,integer_array)
-
-        END SUBROUTINE INTEGER_GROW
-
-        SUBROUTINE INTEGER_GROW2_reverse(integer_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          INTEGER, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: integer_array
-          INTEGER, DIMENSION(:,:), ALLOCATABLE :: integer_tmp
-          INTEGER lSIZE, lSIZE2
-
-          lSIZE = size(integer_array,1)
-          lSIZE2 = size(integer_array,2)
-          allocate(integer_tmp(new_size,lSIZE2))
-          integer_tmp(1:lSIZE,:) = integer_array(1:lSIZE,:)
-          call move_alloc(integer_tmp,integer_array)
-
-        END SUBROUTINE INTEGER_GROW2_reverse
-
-        SUBROUTINE INTEGER_GROW2(integer_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          INTEGER, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: integer_array
-          INTEGER, DIMENSION(:,:), ALLOCATABLE :: integer_tmp
-          INTEGER lSIZE, lSIZE2
-
-          lSIZE = size(integer_array,1)
-          lSIZE2 = size(integer_array,2)
-          allocate(integer_tmp(lSIZE,new_size))
-          integer_tmp(:,1:lSIZE2) = integer_array(:,1:lSIZE2)
-          call move_alloc(integer_tmp,integer_array)
-
-        END SUBROUTINE INTEGER_GROW2
-
-        SUBROUTINE LOGICAL_GROW(logical_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          LOGICAL, DIMENSION(:), ALLOCATABLE, INTENT(INOUT) :: logical_array
-          LOGICAL, DIMENSION(:), ALLOCATABLE :: logical_tmp
-          INTEGER lSIZE2
-
-          lSIZE2 = size(logical_array,1)
-          allocate(logical_tmp(new_size))
-          logical_tmp(1:lSIZE2) = logical_array(1:lSIZE2)
-          call move_alloc(logical_tmp,logical_array)
-
-        END SUBROUTINE LOGICAL_GROW
-
-        SUBROUTINE REAL_GROW(real_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          DOUBLE PRECISION, DIMENSION(:), ALLOCATABLE, INTENT(INOUT) :: real_array
-          DOUBLE PRECISION, DIMENSION(:), ALLOCATABLE :: real_tmp
-          INTEGER lSIZE
-
-          lSIZE = size(real_array,1)
-          allocate(real_tmp(new_size))
-          real_tmp(1:lSIZE) = real_array(1:lSIZE)
-          call move_alloc(real_tmp,real_array)
-
-        END SUBROUTINE REAL_GROW
-
-        SUBROUTINE REAL_GROW2(real_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: real_array
-          DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: real_tmp
-          INTEGER lSIZE, lSIZE2
-
-          lSIZE = size(real_array,1)
-          lSIZE2 = size(real_array,2)
-          allocate(real_tmp(lSIZE,new_size))
-          real_tmp(:,1:lSIZE2) = real_array(:,1:lSIZE2)
-          call move_alloc(real_tmp,real_array)
-
-        END SUBROUTINE REAL_GROW2
-
-        SUBROUTINE REAL_GROW2_reverse(real_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: real_array
-          DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: real_tmp
-          INTEGER lSIZE, lSIZE2
-
-          lSIZE = size(real_array,1)
-          lSIZE2 = size(real_array,2)
-          allocate(real_tmp(new_size,lSIZE2))
-          real_tmp(1:lSIZE,:) = real_array(1:lSIZE,:)
-          call move_alloc(real_tmp,real_array)
-
-        END SUBROUTINE REAL_GROW2_REVERSE
-
-        SUBROUTINE REAL_GROW3(real_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          DOUBLE PRECISION, DIMENSION(:,:,:), ALLOCATABLE, INTENT(INOUT) :: real_array
-          DOUBLE PRECISION, DIMENSION(:,:,:), ALLOCATABLE :: real_tmp
-          INTEGER lSIZE, lSIZE2, lSIZE3
-
-          lSIZE = size(real_array,1)
-          lSIZE2 = size(real_array,2)
-          lSIZE3 = size(real_array,3)
-          allocate(real_tmp(lSIZE,lSIZE2,new_size))
-          real_tmp(:,:,1:lSIZE3) = real_array(:,:,1:lSIZE3)
-          call move_alloc(real_tmp,real_array)
-
-        END SUBROUTINE REAL_GROW3
-
-        SUBROUTINE LOGICAL_GROW2_REVERSE(real_array,new_size)
-          IMPLICIT NONE
-
-          INTEGER, INTENT(IN) :: new_size
-          LOGICAL, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: real_array
-          LOGICAL, DIMENSION(:,:), ALLOCATABLE :: real_tmp
-          INTEGER lSIZE, lSIZE2
-
-          lSIZE = size(real_array,1)
-          lSIZE2 = size(real_array,2)
-          allocate(real_tmp(new_size,lSIZE2))
-          real_tmp(1:lSIZE,:) = real_array(1:lSIZE,:)
-          call move_alloc(real_tmp,real_array)
-
-        END SUBROUTINE LOGICAL_GROW2_REVERSE
-
       END SUBROUTINE PARTICLE_GROW
+
+      SUBROUTINE INTEGER_GROW(integer_array,new_size)
+        IMPLICIT NONE
+
+        INTEGER, INTENT(IN) :: new_size
+        INTEGER, DIMENSION(:), ALLOCATABLE, INTENT(INOUT) :: integer_array
+        INTEGER, DIMENSION(:), ALLOCATABLE :: integer_tmp
+        INTEGER lSIZE
+
+        lSIZE = size(integer_array,1)
+        allocate(integer_tmp(new_size))
+        integer_tmp(1:lSIZE) = integer_array(1:lSIZE)
+        call move_alloc(integer_tmp,integer_array)
+
+      END SUBROUTINE INTEGER_GROW
+
+      SUBROUTINE INTEGER_GROW2_reverse(integer_array,new_size)
+        IMPLICIT NONE
+
+        INTEGER, INTENT(IN) :: new_size
+        INTEGER, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: integer_array
+        INTEGER, DIMENSION(:,:), ALLOCATABLE :: integer_tmp
+        INTEGER lSIZE, lSIZE2
+
+        lSIZE = size(integer_array,1)
+        lSIZE2 = size(integer_array,2)
+        allocate(integer_tmp(new_size,lSIZE2))
+        integer_tmp(1:lSIZE,:) = integer_array(1:lSIZE,:)
+        call move_alloc(integer_tmp,integer_array)
+
+      END SUBROUTINE INTEGER_GROW2_reverse
+
+      SUBROUTINE INTEGER_GROW2(integer_array,new_size)
+        IMPLICIT NONE
+
+        INTEGER, INTENT(IN) :: new_size
+        INTEGER, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: integer_array
+        INTEGER, DIMENSION(:,:), ALLOCATABLE :: integer_tmp
+        INTEGER lSIZE, lSIZE2
+
+        lSIZE = size(integer_array,1)
+        lSIZE2 = size(integer_array,2)
+        allocate(integer_tmp(lSIZE,new_size))
+        integer_tmp(:,1:lSIZE2) = integer_array(:,1:lSIZE2)
+        call move_alloc(integer_tmp,integer_array)
+
+      END SUBROUTINE INTEGER_GROW2
+
+      SUBROUTINE LOGICAL_GROW(logical_array,new_size)
+        IMPLICIT NONE
+
+        INTEGER, INTENT(IN) :: new_size
+        LOGICAL, DIMENSION(:), ALLOCATABLE, INTENT(INOUT) :: logical_array
+        LOGICAL, DIMENSION(:), ALLOCATABLE :: logical_tmp
+        INTEGER lSIZE2
+
+        lSIZE2 = size(logical_array,1)
+        allocate(logical_tmp(new_size))
+        logical_tmp(1:lSIZE2) = logical_array(1:lSIZE2)
+        call move_alloc(logical_tmp,logical_array)
+
+      END SUBROUTINE LOGICAL_GROW
+
+      SUBROUTINE REAL_GROW(real_array,new_size)
+        IMPLICIT NONE
+
+        INTEGER, INTENT(IN) :: new_size
+        DOUBLE PRECISION, DIMENSION(:), ALLOCATABLE, INTENT(INOUT) :: real_array
+        DOUBLE PRECISION, DIMENSION(:), ALLOCATABLE :: real_tmp
+        INTEGER lSIZE
+
+        lSIZE = size(real_array,1)
+        allocate(real_tmp(new_size))
+        real_tmp(1:lSIZE) = real_array(1:lSIZE)
+        call move_alloc(real_tmp,real_array)
+
+      END SUBROUTINE REAL_GROW
+
+      SUBROUTINE REAL_GROW2(real_array,new_size)
+        IMPLICIT NONE
+
+        INTEGER, INTENT(IN) :: new_size
+        DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: real_array
+        DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: real_tmp
+        INTEGER lSIZE, lSIZE2
+
+        lSIZE = size(real_array,1)
+        lSIZE2 = size(real_array,2)
+        allocate(real_tmp(lSIZE,new_size))
+        real_tmp(:,1:lSIZE2) = real_array(:,1:lSIZE2)
+        call move_alloc(real_tmp,real_array)
+
+      END SUBROUTINE REAL_GROW2
+
+      SUBROUTINE REAL_GROW2_reverse(real_array,new_size)
+        IMPLICIT NONE
+
+        INTEGER, INTENT(IN) :: new_size
+        DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: real_array
+        DOUBLE PRECISION, DIMENSION(:,:), ALLOCATABLE :: real_tmp
+        INTEGER lSIZE, lSIZE2
+
+        lSIZE = size(real_array,1)
+        lSIZE2 = size(real_array,2)
+        allocate(real_tmp(new_size,lSIZE2))
+        real_tmp(1:lSIZE,:) = real_array(1:lSIZE,:)
+        call move_alloc(real_tmp,real_array)
+
+      END SUBROUTINE REAL_GROW2_REVERSE
+
+      SUBROUTINE REAL_GROW3(real_array,new_size)
+        IMPLICIT NONE
+
+        INTEGER, INTENT(IN) :: new_size
+        DOUBLE PRECISION, DIMENSION(:,:,:), ALLOCATABLE, INTENT(INOUT) :: real_array
+        DOUBLE PRECISION, DIMENSION(:,:,:), ALLOCATABLE :: real_tmp
+        INTEGER lSIZE, lSIZE2, lSIZE3
+
+        lSIZE = size(real_array,1)
+        lSIZE2 = size(real_array,2)
+        lSIZE3 = size(real_array,3)
+        allocate(real_tmp(lSIZE,lSIZE2,new_size))
+        real_tmp(:,:,1:lSIZE3) = real_array(:,:,1:lSIZE3)
+        call move_alloc(real_tmp,real_array)
+
+      END SUBROUTINE REAL_GROW3
+
+      SUBROUTINE LOGICAL_GROW2_REVERSE(real_array,new_size)
+        IMPLICIT NONE
+
+        INTEGER, INTENT(IN) :: new_size
+        LOGICAL, DIMENSION(:,:), ALLOCATABLE, INTENT(INOUT) :: real_array
+        LOGICAL, DIMENSION(:,:), ALLOCATABLE :: real_tmp
+        INTEGER lSIZE, lSIZE2
+
+        lSIZE = size(real_array,1)
+        lSIZE2 = size(real_array,2)
+        allocate(real_tmp(new_size,lSIZE2))
+        real_tmp(1:lSIZE,:) = real_array(1:lSIZE,:)
+        call move_alloc(real_tmp,real_array)
+
+      END SUBROUTINE LOGICAL_GROW2_REVERSE
 
     END MODULE DES_ALLOCATE
