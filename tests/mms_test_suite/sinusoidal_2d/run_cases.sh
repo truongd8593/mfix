a5676a4d5a3dfe540391cc22a2af898beedcfbe4
Aniruddha Choudhary <aniruddha.choudhary@contr.netl.doe.gov>
2015-02-10 15:06:52 -0500

    Fix problem with compilation crash; Sinusoidal_2d cases run fine.

--- tests/mms_test_suite/sinusoidal_2d/run_cases.sh
+++ tests/mms_test_suite/sinusoidal_2d/run_cases.sh
@@ -2,8 +2,8 @@
 export CASE_DIR=`pwd`
 
 # load modules
-#module load gnu/4.6.4 openmpi/1.5.5_gnu4.6
-module load intel/2013.5.192 intelmpi/4.1.1.036
+module load gnu/4.6.4 openmpi/1.5.5_gnu4.6
+#module load intel/2013.5.192 intelmpi/4.1.1.036
 
 # copy common files to ./src/
 cp ../usr_common/usr_mod.f ./src/usr_mod.f
@@ -37,29 +37,29 @@ cat $CASE_DIR/de_norms.dat >> $CASE_DIR/de_norms_collected.dat
 rm $CASE_DIR/{MMS2D.*,de_norms.dat,out.log}
 
 # Run mesh_32 (i.e., 32x32 for 2D, 32x32x32 for 3D)
-#echo "******** Running mesh_32..."
-#mpirun -np 8 $CASE_DIR/src/mfix.exe imax=32 jmax=32 nodesi=4 nodesj=2 nodesk=1 > out.log
-#cat $CASE_DIR/de_norms.dat >> $CASE_DIR/de_norms_collected.dat
-#rm $CASE_DIR/{MMS2D.*,de_norms.dat,out.log}
+echo "******** Running mesh_32..."
+mpirun -np 8 $CASE_DIR/src/mfix.exe imax=32 jmax=32 nodesi=4 nodesj=2 nodesk=1 > out.log
+cat $CASE_DIR/de_norms.dat >> $CASE_DIR/de_norms_collected.dat
+rm $CASE_DIR/{MMS2D.*,de_norms.dat,out.log}
 
 # Run mesh_64 (i.e., 64x64 for 2D, 64x64x64 for 3D)
-#echo "******** Running mesh_64..."
-#mpirun -np 16 $CASE_DIR/src/mfix.exe imax=64 jmax=64 nodesi=8 nodesj=2 nodesk=1 > out.log
-#cat $CASE_DIR/de_norms.dat >> $CASE_DIR/de_norms_collected.dat
-#rm $CASE_DIR/{MMS2D.*,de_norms.dat,out.log}
-#
-## Run mesh_128 (i.e., 128x128 for 2D, 128x128x128 for 3D)
-#echo "******** Running mesh_128..."
-#mpirun -np 16 $CASE_DIR/src/mfix.exe imax=128 jmax=128 nodesi=8 nodesj=2 nodesk=1 > out.log
-#cat $CASE_DIR/de_norms.dat >> $CASE_DIR/de_norms_collected.dat
-#rm $CASE_DIR/{MMS2D.*,de_norms.dat,out.log}
-#
-## Evaluate observed orders
+echo "******** Running mesh_64..."
+mpirun -np 16 $CASE_DIR/src/mfix.exe imax=64 jmax=64 nodesi=8 nodesj=2 nodesk=1 > out.log
+cat $CASE_DIR/de_norms.dat >> $CASE_DIR/de_norms_collected.dat
+rm $CASE_DIR/{MMS2D.*,de_norms.dat,out.log}
+
+# Run mesh_128 (i.e., 128x128 for 2D, 128x128x128 for 3D)
+echo "******** Running mesh_128..."
+mpirun -np 16 $CASE_DIR/src/mfix.exe imax=128 jmax=128 nodesi=8 nodesj=2 nodesk=1 > out.log
+cat $CASE_DIR/de_norms.dat >> $CASE_DIR/de_norms_collected.dat
+rm $CASE_DIR/{MMS2D.*,de_norms.dat,out.log}
+
+# Evaluate observed orders
 cp ../usr_common/ooa_test.f95 $CASE_DIR
 echo "******** Calculating observed orders..."
 gfortran -o ooa_test ooa_test.f95
 ./ooa_test
-#rm $CASE_DIR/{ooa_test,ooa_test.f95,de_norms_collected.dat}
+rm $CASE_DIR/{ooa_test,ooa_test.f95,de_norms_collected.dat}
 mv $CASE_DIR/de_l2.dat $CASE_DIR/AUTOTEST/de_l2.dat
 mv $CASE_DIR/de_linf.dat $CASE_DIR/AUTOTEST/de_linf.dat
 mv $CASE_DIR/ooa_l2.dat $CASE_DIR/AUTOTEST/ooa_l2.dat
@@ -67,7 +67,6 @@ mv $CASE_DIR/ooa_linf.dat $CASE_DIR/AUTOTEST/ooa_linf.dat
 
 rm $CASE_DIR/src/usr_mod.f
 rm $CASE_DIR/src/usr3.f
-rm $CASE_DIR/{ooa_test.f95,ooa_test}
-#rm $CASE_DIR/src/mfix.exe
+rm $CASE_DIR/src/mfix.exe
 
 echo "******** Done."
