47ec7d822aa8af6d03e85a32ab144d051f5dc59b
Jordan Musser <jordan.musser@netl.doe.gov>
2015-03-02 08:52:28 -0500

    Added dummy stub function for write_des_data

---
+++ post_mfix/des/write_des_data.f
@@ -0,0 +1,22 @@
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv!
+!                                                                      !
+!  Subroutine: WRITE_DES_DATA                                          !
+!  Purpose: Writing DES output in Paraview format                      !
+!                                                                      !
+!                                                                      !
+!  Author: Jay Boyalakuntla                           Date: 26-Jul-06  !
+!  Reviewer: Sreekanth Pannala                        Date: 31-Oct-06  !
+!                                                                      !
+!  Reviewer: J. Musser                                Date: 20-Apr-10  !
+!  Comments: Split original subroutine into one for ParaView *.vtp     !
+!  files, and a second for TECPLOT files *.dat.                        !
+!                                                                      !
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^!
+      SUBROUTINE WRITE_DES_DATA
+
+      IMPLICIT NONE
+
+! Dummy stub function for POST MFIX
+
+      RETURN
+      END SUBROUTINE WRITE_DES_DATA
