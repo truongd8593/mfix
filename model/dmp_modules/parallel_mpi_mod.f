b9ce52486a592be2cae15d861c42fd212b64d34e
Mark Meredith <mark.meredith@contr.netl.doe.gov>
2015-04-28 22:43:58 -0400

    dmp fixes

--- model/dmp_modules/parallel_mpi_mod.f
+++ model/dmp_modules/parallel_mpi_mod.f
@@ -9,54 +9,59 @@
         contains
 
         subroutine parallel_init()
+          implicit none
+          integer :: ierr
 
-        integer :: ierr
+          numPEs = 1
+          myPE = 0
 
-        numPEs = 1
-        myPE = 0
 #ifdef MPI
+          call MPI_Init(ierr)
+          call MPI_Check( 'parallel_init:MPI_Init ', ierr)
 
-        call MPI_Init(ierr)
-        call MPI_Check( 'parallel_init:MPI_Init ', ierr)
+          call MPI_COMM_SIZE( MPI_COMM_WORLD, numPEs, ierr )
+          call MPI_Check( 'parallel_init:MPI_Comm_size ', ierr )
 
-        call MPI_COMM_SIZE( MPI_COMM_WORLD, numPEs, ierr )
-        call MPI_Check( 'parallel_init:MPI_Comm_size ', ierr )
-
-        call MPI_COMM_RANK( MPI_COMM_WORLD, myPE, ierr )
-        call MPI_Check( 'parallel_init:MPI_Comm_size ', ierr )
+          call MPI_COMM_RANK( MPI_COMM_WORLD, myPE, ierr )
+          call MPI_Check( 'parallel_init:MPI_Comm_size ', ierr )
 #endif
-        return
+          return
         end subroutine parallel_init
 
         subroutine parallel_fin()
+          implicit none
 
 #ifdef MPI
-        integer :: ierr
+          integer :: ierr
 
-        call MPI_Finalize(ierr)
-        call MPI_Check( 'parallel_init:MPI_Finalize ', ierr)
+          call MPI_Finalize(ierr)
+          call MPI_Check( 'parallel_init:MPI_Finalize ', ierr)
 #endif
 
-        return
+          return
         end subroutine parallel_fin
 
         subroutine MPI_Check( msg, ierr )
-        character(len=*),intent(in) :: msg
-        integer, intent(in) :: ierr
+#ifdef MPI
+          use mpi, only MPI_SUCCESS
+#endif
+          implicit none
+          character(len=*),intent(in) :: msg
+          integer, intent(in) :: ierr
 
 #ifdef MPI
-        character(len=512) :: errmsg
-        integer :: resultlen, ierror
+          character(len=512) :: errmsg
+          integer :: resultlen, ierror
 
-        if (ierr .ne. MPI_SUCCESS ) then
-                call MPI_Error_string( ierr, errmsg, resultlen, ierror )
-                print*, 'Error: ', msg
-                print*, errmsg(1:resultlen)
-                stop '** ERROR ** '
-        endif
+          if (ierr .ne. MPI_SUCCESS ) then
+             call MPI_Error_string( ierr, errmsg, resultlen, ierror )
+             print*, 'Error: ', msg
+             print*, errmsg(1:resultlen)
+             stop '** ERROR ** '
+          endif
 #endif
 
-        return
+          return
         end subroutine MPI_Check
 
 
