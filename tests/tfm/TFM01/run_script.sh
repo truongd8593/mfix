741c994ea38f7b9f2138fb28de5c24c5edd36ff0
Aniruddha Choudhary <aniruddha.choudhary@contr.netl.doe.gov>
2015-04-30 14:14:48 -0400

    Intial commit to TFM tests with channel case.

---
+++ tests/tfm/TFM01/run_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -exl
+
+# set case directory
+export CASE_DIR=`pwd`
+
+# load modules
+module load gnu/4.6.4 openmpi/1.5.5_gnu4.6
+
+# compile MFIX in ./src/
+echo "******** Compiling MFIX..."
+cd $CASE_DIR
+../../../model/make_mfix --dmp --opt=O3 --compiler=gcc --exe=mfix.exe -j
+
+cd $CASE_DIR
+
+# Run mesh_32 (i.e., 32x32 for 2D, 32x32x32 for 3D)
+echo "******** Running mesh_32..."
+mpirun -np 8 $CASE_DIR/mfix.exe imax=32 jmax=32 nodesi=4 nodesj=2 nodesk=1 > out.log
+rm $CASE_DIR/{TFM01.*,out.log}
+
+rm $CASE_DIR/mfix.exe
+
+echo "******** Done."
