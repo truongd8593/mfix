a30f35cf10dc905abedb48505f77ab4ac1f4aa06
Jordan Musser <jordan.musser@netl.doe.gov>
2015-06-12 09:11:56 -0400

    dem-tests/terminal_velocity -> tests/dem/DEM06
    Refine and expand the DEM terminal velocity test case

---
+++ tests/dem/DEM06/usr0.f
@@ -0,0 +1,34 @@
+!vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvC
+!                                                                      C
+!  Module name: USR0                                                   C
+!                                                                      C
+!  Purpose: This routine is called before the time loop starts and is  C
+!           user-definable.  The user may insert code in this routine  C
+!           or call appropriate user defined subroutines.  This        C
+!           can be used for setting constants and checking errors in   C
+!           data.  This routine is not called from an IJK loop, hence  C
+!           all indices are undefined.                                 C
+!                                                                      C
+!^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^C
+      SUBROUTINE USR0
+
+      use usr
+      use discretelement, only: PIP
+      use discretelement, only: DES_POS_NEW
+      use discretelement, only: DES_VEL_NEW
+
+      IMPLICIT NONE
+
+      INTEGER :: NP
+
+      IF(PIP /= 1) THEN
+         write(*,"(3x, 'invalid setup for test case')")
+         call mfix_exit(0)
+      ENDIF
+
+! Store the initial position and velocity
+      RK4_POS(:) = DES_POS_NEW(:,1)
+      RK4_VEL(:) = DES_VEL_NEW(:,1)
+
+      return
+      END SUBROUTINE USR0
