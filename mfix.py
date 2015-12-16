55fa340dd2df90511210f6beec2d36199f8fdb3c
Mark Meredith <mark.meredith@netl.doe.gov>
2015-12-15 21:18:01 -0500

    add command to set variables (for optional MPI-global index range) add command to list variables that can be set

--- mfix.py
+++ mfix.py
@@ -47,12 +47,21 @@ def handle(req_id,command):
     print "THE COMMAND IS",cmd
     if cmd=='help':
         responses[req_id] = '''Usage:
-        stop          - stop mfix
-        go            - (re)start mfix
-        hello         - say hello from each rank
-        step          - execute one timestep
-        set VAR=VALUE - set variable to value
+        go                                  - (re)start mfix
+        list                                - list variables that can be set
+        set VAR=VALUE [ i1 j1 k1 i2 j2 k2 ] - set variable to value [ for a certain index range ]
+        step                                - execute one timestep
+        stop                                - stop mfix
         \n'''
+
+    elif cmd=='list':
+        resp = ''
+        for module in ('compar','discretelement','fldvar','main','param','run','run_dp'):
+            attr = getattr(pymfix,module)
+            resp = resp + 'in module %s you can set attributes %s \n\n' % (module,dir(attr))
+        resp = resp + " Except for those of the above that are really subroutines/functions instead of variables. Also, the above list is incomplete, and is missing most of the interesting variables. \n\n  Don't forget to prefix with pymfix; e.g. set pymfix.run.dt 0.001\n\n"
+        responses[req_id] = resp
+        mfix_stopped = True
 
     elif cmd=='stop':
         responses[req_id] = 'STOPPING MFIX\n'
@@ -62,16 +71,42 @@ def handle(req_id,command):
         responses[req_id] = 'STARTING MFIX\n'
         mfix_stopped = False
 
-    elif cmd=='hello':
-        print("HELLO FROM MPI RANK %d" % (pymfix.compar.mype))
-        responses[req_id] = 'PRINTING MPI RANK INFO TO STDOUT\n'
-
     elif cmd=='step':
         pymfix.main.step()
         responses[req_id] = 'DOING ONE TIMESTEP\n'
 
+    elif cmd=='set':
+        pymfix.main.step()
+        var = command.split(' ')[1].lower().strip()
+        val = command.split(' ')[2].lower().strip()
+
+        # slow things down for development
+        time.sleep(pymfix.compar.mype)
+
+        if len(command.split(' ')) > 3:
+            ii,jj,kk,i2,j2,k2 = command.split(' ')[3:9]
+            for i in range(int(ii),int(i2)+1):
+                for j in range(int(jj),int(j2)+1):
+                    for k in range(int(kk),int(k2)+1):
+                        if is_on_mype_owns(i,j,k):
+                            exec_string = ('%s[%d] = %s' % (var,funijk(i,j,k),val))
+                            print "GOING TO EXECUTE: ",exec_string
+                            exec(exec_string)
+                            print "rank ",pymfix.compar.mype," set value for",i,j,k,funijk(i,j,k)
+                        else:
+                            print "rank ",pymfix.compar.mype,"does not own",i,j,k,funijk(i,j,k)
+        else:
+            exec('%s = %s' % (var,val))
+        responses[req_id] = 'ok, I set %s to %s\n' % (var,val)
+
     else:
         responses[req_id] = 'UNRECOGNIZED COMMAND\n'
+
+def funijk(i,j,k):
+    return pymfix.compar.ijk_array_of[i,j,k]
+
+def is_on_mype_owns(li, lj, lk):
+      return li >= pymfix.compar.istart and li <= pymfix.compar.iend and lj >= pymfix.compar.jstart and lj <= pymfix.compar.jend and lk >= pymfix.compar.kstart and lk <= pymfix.compar.kend
 
 class Echo(protocol.Protocol):
     """This is just about the simplest possible protocol"""
