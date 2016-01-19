a602a5cce6c169bcfa36648a12c024e9b8319df5
Jordan Musser <jordan.musser@netl.doe.gov>
2016-01-19 07:02:41 -0500

    Bug fix for DEM mass inflows
    Entering ghost particles need to be relcassified as ghost (not
    normal) after fully entering the domain.

--- model/des/mass_inflow_dem.f
+++ model/des/mass_inflow_dem.f
@@ -50,7 +50,10 @@
                CASE('B'); DIST = ZT(BC_K_b(BCV)-1) - DES_POS_NEW(3,NP)
                END SELECT
 ! The particle is still inside the domain
-               IF(DIST > DES_RADIUS(NP)) CALL SET_NORMAL(NP)
+               IF(DIST > DES_RADIUS(NP)) THEN
+                  IF(IS_ENTERING(NP)) CALL SET_NORMAL(NP)
+                  IF(IS_ENTERING_GHOST(NP)) CALL SET_GHOST(NP)
+               ENDIF
             ENDDO
          ENDDO
 
