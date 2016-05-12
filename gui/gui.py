962913cb3eba66c2cd6326254c1b68306e4112f9
Eli Heady <eli.heady@gmail.com>
2016-05-12 22:33:21 +0000

    last format to interpolation fix

--- gui/gui.py
+++ gui/gui.py
@@ -1305,8 +1305,7 @@ class MfixGui(QtWidgets.QMainWindow):
             self.message(
                 title='Warning',
                 icon='warning',
-                text = "The directory %s is not writable" % directory
-                text=(dialog_text),
+                text="The directory %s is not writable" % directory,
                 buttons=['ok'],
                 default='ok')
 
