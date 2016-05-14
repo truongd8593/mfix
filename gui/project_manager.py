23aacc9e345497edb80f9b2f53d1e342321bdb3b
Charles G Waldman <cgw@alum.mit.edu>
2016-05-14 11:46:46 -0500

    rename, parent->gui

--- gui/project_manager.py
+++ gui/project_manager.py
@@ -11,11 +11,11 @@ from tools.general import format_key_with_args
 
 # --- Project Manager ---
 class ProjectManager(Project):
-    """handles interaction between gui (parent) and mfix project (Project)"""
+    """handles interaction between gui and mfix project"""
 
-    def __init__(self, parent=None, keyword_doc=None):
+    def __init__(self, gui=None, keyword_doc=None):
         Project.__init__(self, keyword_doc=keyword_doc)
-        self.parent = parent
+        self.gui = gui
         self.keyword_and_args_to_widget = {}
         self.registered_keywords = set()
         self._widget_update_stack = [] # prevent circular updates
@@ -69,7 +69,7 @@ class ProjectManager(Project):
         try:
             updatedValue = self.updateKeyword(key, newValue, args)
         except Exception as e:
-            self.parent.print_internal("Warning: %s: %s" %
+            self.gui.print_internal("Warning: %s: %s" %
                                        (format_key_with_args(key, args), e),
                                        color='red')
             return
@@ -102,13 +102,13 @@ class ProjectManager(Project):
                 #log.warn("%s: %s" % (e, ka))
                 msg = "Cannot set %s = %s: %s" % (ka, updatedValue, e)
                 if widget: # We're in a callback, not loading
-                    self.parent.print_internal(msg, color='red')
+                    self.gui.print_internal(msg, color='red')
                 raise ValueError(msg)
 
             finally:
                 self._widget_update_stack.pop()
 
-        self.parent.print_internal("%s = %s" % (format_key_with_args(key, args),
+        self.gui.print_internal("%s = %s" % (format_key_with_args(key, args),
                                                 updatedValue),
                                    font="Monospace", color='red' if warn else None)
 
@@ -132,7 +132,7 @@ class ProjectManager(Project):
             return HYBRID
         # mfix settings are inconsistent, warn user.  (Popup here?)
         msg = "Warning, cannot deduce solver type"
-        self.parent.print_internal(msg, color='red')
+        self.gui.print_internal(msg, color='red')
         log.warn(msg)
         #default
         return SINGLE
@@ -151,9 +151,9 @@ class ProjectManager(Project):
             # Let's guess the solver type from the file
             self.solver = self.guess_solver()
             # Now put the GUI into the correct state before setting up interface
-            self.parent.set_solver(self.solver)
+            self.gui.set_solver(self.solver)
 
-            # deal with species, since they cause other keywords to be defined
+            # deal with species, since they can cause other widgets to instantiate
             nmax_g = self.get_value('nmax_g', 0)
             for i in range(1, nmax_g+1):
                 species = self.get_value(['species_g', i])
@@ -170,14 +170,14 @@ class ProjectManager(Project):
 
             # report any errors
             for w in errlist + ws:
-                self.parent.print_internal("Warning: %s" % w.message, color='red')
+                self.gui.print_internal("Warning: %s" % w.message, color='red')
             n_errs = len(errlist) + len(ws)
             if n_errs:
-                self.parent.print_internal("Warning: %s loading %s" %
+                self.gui.print_internal("Warning: %s loading %s" %
                                            (plural(n_errs, "error") , project_file),
                                            color='red')
             else:
-                self.parent.print_internal("Loaded %s" % project_file, color='blue')
+                self.gui.print_internal("Loaded %s" % project_file, color='blue')
 
     def register_widget(self, widget, keys=None, args=None):
         '''
