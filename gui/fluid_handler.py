99e498fbc838c69a39f0c37753b7164e9cdfe687
Charles G Waldman <cgw@alum.mit.edu>
2016-05-25 19:30:15 -0500

    move fluid-handling code from gui.py to fluid_handler.py

---
+++ gui/fluid_handler.py
@@ -0,0 +1,205 @@
+# methods to deal with fluids, split off from gui.py
+
+from __future__ import print_function, absolute_import, unicode_literals, division
+
+from copy import deepcopy
+
+#import Qt
+from qtpy import QtCore, QtWidgets, QtGui, PYQT4, PYQT5
+from qtpy.QtCore import (QObject, QThread, pyqtSignal, QUrl, QSettings,
+                         Qt)
+
+#local imports
+from constants import *
+from tools.general import set_item_noedit, get_selected_row
+
+class FluidHandler:
+        ## Fluid phase methods
+    def enable_fluid_species_eq(self, state):
+        ui = self.ui
+        for item in (ui.combobox_fluid_diffusion_model,
+                     # more ?
+                     ):
+            item.setEnabled(state)
+        spinbox = ui.spinbox_keyword_dif_g0 # dif_g0 == diffusion coeff model
+        if state:
+            spinbox.setEnabled(self.fluid_diffusion_model == CONSTANT)
+        else:
+            spinbox.setEnabled(False)
+
+    def enable_fluid_scalar_eq(self, state):
+        spinbox = self.ui.spinbox_fluid_nscalar_eq
+        spinbox.setEnabled(state)
+        if state:
+            val = spinbox.value()
+            self.set_fluid_nscalar_eq(val)
+        else:
+            # Don't call set_fluid_nscalar_eq(0) b/c that will clobber spinbox
+            prev_nscalar = self.fluid_nscalar_eq + self.solid_nscalar_eq
+            self.fluid_nscalar_eq = 0
+            self.update_scalar_equations(prev_nscalar)
+
+    def set_fluid_nscalar_eq(self, value):
+        # This *sums into* nscalar - not a simple keyword
+        prev_nscalar = self.fluid_nscalar_eq + self.solid_nscalar_eq
+        self.fluid_nscalar_eq = value
+        spinbox = self.ui.spinbox_fluid_nscalar_eq
+        if value != spinbox.value():
+            spinbox.setValue(value)
+            return
+        self.update_scalar_equations(prev_nscalar)
+
+    # molecular wt model only has 2 choices, and the key names don't
+    # follow the same pattern, so create its setter specially
+    def set_fluid_mol_weight_model(self, model):
+        self.fluid_mol_weight_model = model
+        combobox = self.ui.combobox_fluid_mol_weight_model
+        prev_model = combobox.currentIndex()
+        if model != prev_model:
+            combobox.setCurrentIndex(model)
+            return
+        # Enable spinbox for constant mol_weight model
+        spinbox = self.ui.spinbox_keyword_mw_avg
+        spinbox.setEnabled(model==CONSTANT)
+        if model == CONSTANT:
+            value = spinbox.value() # Possibly re-enabled gui item
+            if self.project.get_value("mw_avg") != value:
+                self.set_keyword("mw_avg", value) # Restore keyword value
+        else: # Mixture
+            # TODO: validate, require mw for all component species
+            self.unset_keyword("mw_avg")
+
+    def set_fluid_phase_name(self, value):
+        if value != self.ui.lineedit_fluid_phase_name.text():
+            self.ui.lineedit_fluid_phase_name.setText(value)
+            return
+        value = None if value=='Fluid' else value # don't save default
+        self.project.mfix_gui_comments['fluid_phase_name'] = value
+
+    def disable_fluid_solver(self, disabled):
+        enabled = not disabled
+        item = self.find_navigation_tree_item("Fluid")
+        item.setDisabled(disabled)
+        ms = self.ui.model_setup
+        checkbox = ms.checkbox_enable_turbulence
+        checkbox.setEnabled(enabled)
+        ms.combobox_turbulence_model.setEnabled(enabled and
+                                                checkbox.isChecked())
+
+
+    # --- fluid species methods ---
+    def fluid_species_revert(self):
+        self.fluid_species = self.saved_fluid_species
+        self.species_popup.defined_species = deepcopy(self.fluid_species)
+        self.update_fluid_species_table()
+
+    def fluid_species_save(self):
+        self.fluid_species = deepcopy(self.species_popup.defined_species)
+        self.update_fluid_species_table()
+
+    def update_fluid_species_table(self):
+        """Update table in fluids pane.  Also set nmax_g, species_g and species_alias_g keywords,
+        which are not tied to a single widget"""
+
+        hv = QtWidgets.QHeaderView
+        table = self.ui.tablewidget_fluid_species
+        if PYQT5:
+            resize = table.horizontalHeader().setSectionResizeMode
+        else:
+            resize = table.horizontalHeader().setResizeMode
+        for n in range(5):
+            resize(n, hv.ResizeToContents if n>0
+                   else hv.Stretch)
+
+        table.clearContents()
+        if self.fluid_species is None:
+            return
+        nrows = len(self.fluid_species)
+        table.setRowCount(nrows)
+        def make_item(val):
+            item = QtWidgets.QTableWidgetItem('' if val is None else str(val))
+            set_item_noedit(item)
+            return item
+        old_nmax_g = self.project.get_value('nmax_g')
+        nmax_g = len(self.fluid_species)
+        if nmax_g > 0:
+            self.update_keyword('nmax_g', nmax_g)
+        else:
+            self.unset_keyword('nmax_g')
+        for (row, (species,data)) in enumerate(self.fluid_species.items()):
+            for (col, key) in enumerate(('alias', 'phase', 'mol_weight',
+                                        'h_f', 'source')):
+                alias = data.get('alias', species) # default to species if no alias
+                data['alias'] = alias # for make_item
+                table.setItem(row, col, make_item(data.get(key)))
+                self.update_keyword('species_g', species, args=row+1)
+                self.update_keyword('species_alias_g', alias, args=row+1)
+                # We're avoiding mw_g in favor of the settings in THERMO DATA
+                #self.update_keyword('mw_g', data['mol_weight'], args=row+1)#
+
+        # Clear any keywords with indices above nmax_g
+        if old_nmax_g is None:
+            old_nmax_g = 0
+        for i in range(nmax_g+1, old_nmax_g+1):
+            self.unset_keyword('species_g', i)
+            self.unset_keyword('species_alias_g', i)
+            #self.unset_keyword('mw_g', i) # TBD
+
+        self.project.update_thermo_data(self.fluid_species)
+
+    def handle_fluid_species_selection(self):
+        row = get_selected_row(self.ui.tablewidget_fluid_species)
+        enabled = (row is not None)
+        self.ui.toolbutton_fluid_species_delete.setEnabled(enabled)
+        self.ui.toolbutton_fluid_species_copy.setEnabled(enabled)
+
+    def fluid_species_add(self):
+        sp = self.species_popup
+        sp.phases='GL' # ? is this correct
+        # how to avoid this if dialog open already?
+        self.saved_fluid_species = deepcopy(self.fluid_species) # So we can revert
+        sp.cancel.connect(self.fluid_species_revert)
+        sp.save.connect(self.fluid_species_save)
+        sp.defined_species = self.fluid_species
+        sp.update_defined_species()
+        sp.setWindowTitle("Fluid Species")
+        sp.show()
+        sp.raise_()
+        sp.activateWindow()
+
+    def fluid_species_delete(self):
+        # XXX FIXME this is potentially a big problem since
+        # it results in species being renumbered, or a hole in
+        # the sequence - either way is trouble.  Have to warn
+        # user, if species is referenced elsewhere.
+        table = self.ui.tablewidget_fluid_species
+        row = get_selected_row(table)
+        if row is None: # No selection
+            return
+        table.clearSelection()
+        key = self.fluid_species.keys()[row]
+        del self.fluid_species[key]
+        self.update_fluid_species_table()
+        # Sigh, we have to update the row in the popup too.
+        # Should the popup just be modal, to avoid this?
+        sp = self.species_popup
+        sp.defined_species = self.fluid_species
+        sp.update_defined_species()
+
+    def fluid_species_edit(self):
+        table = self.ui.tablewidget_fluid_species
+        row = get_selected_row(table)
+        sp = self.species_popup
+        self.saved_fluid_species = deepcopy(self.fluid_species) # So we can revert
+        sp.cancel.connect(self.fluid_species_revert)
+        sp.save.connect(self.fluid_species_save)
+        sp.defined_species = self.fluid_species
+        sp.update_defined_species()
+        if row is None:
+            sp.tablewidget_defined_species.clearSelection()
+        else:
+            sp.tablewidget_defined_species.setCurrentCell(row, 0)
+        sp.setWindowTitle("Fluid Species")
+        sp.show()
+        sp.raise_()
+        sp.activateWindow()
