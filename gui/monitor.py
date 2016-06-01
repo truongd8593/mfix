02b2332909ae133c8059290bdc4289be876514eb
Charles G Waldman <cgw@alum.mit.edu>
2016-06-01 18:13:19 -0500

    new file 'monitor.py' split off from mfix_threads

---
+++ gui/monitor.py
@@ -0,0 +1,90 @@
+"""classes to monitor MFIX output files & executables"""
+
+import os
+import time
+import glob
+import json
+import logging
+import subprocess
+
+log = logging.getLogger(__name__)
+
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen, Request
+    from urllib.parse import urlencode
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib import urlencode
+    from urllib2 import urlopen, Request
+
+
+class Monitor(object):
+    """class for monitoring available MFIX executables and output files"""
+    # TODO move exe_watcher stuff from gui.py here
+    def __init__(self, parent):
+        self.parent = parent
+        self.cache = {}
+        self.outputs = None
+        self.exes = {}
+        self.update_exes()
+
+    def get_exes(self):
+        """returns a dict mapping full [mfix|pymfix] paths
+        to configuration options."""
+        self.update_exes()
+        return self.exes
+
+    def update_exes(self):
+        """update self.exes"""
+        def mfix_print_flags(mfix_exe, cache=self.cache):
+            """Determine mfix configuration by running mfix --print-flags.  Cache results"""
+            try: # Possible race, file may have been deleted/renamed since isfile check!
+                stat = os.stat(mfix_exe)
+            except OSError as err:
+                log = logging.getLogger(__name__)
+                log.exception("could not run %s --print-flags", mfix_exe)
+                return ''
+
+            cached_stat, cached_flags = cache.get(mfix_exe, (None, None))
+            if cached_stat and cached_stat == stat:
+                return cached_flags
+
+            exe_dir = os.path.dirname(mfix_exe)
+            popen = subprocess.Popen(mfix_exe + " --print-flags",
+                                     cwd=exe_dir,
+                                     stdout=subprocess.PIPE,
+                                     stderr=subprocess.PIPE,
+                                     shell=True)
+            (out, err) = popen.communicate()
+            flags = '' if err else out.strip()
+            cache[mfix_exe] = (stat, flags)
+            return flags
+
+        exes = {} # map exes -> flags
+        for d in self.parent.exe_watcher.directories():
+            for name in 'mfix', 'mfix.exe', 'pymfix', 'pymfix.exe':
+                exe = os.path.abspath(os.path.join(d, name))
+                if os.path.isfile(exe):
+                    log.debug("found %s executable in %s" %( name, d))
+                    exes[exe] = str(mfix_print_flags(exe))
+        self.exes = exes
+
+    def get_res_files(self): # Why is this in monitor class?
+        if not self.parent.get_project_dir():
+            return
+        pattern = os.path.join(self.parent.get_project_dir(), '*.RES')
+        return glob.glob(pattern)
+
+    def get_outputs(self, patterns=[]):
+        project_dir = self.parent.get_project_dir()
+        if project_dir is None:
+            return
+        if len(patterns) == 0:
+            patterns = [
+                '*.LOG', '*.OUT', '*.RES', '*.SP?', 'MFIX.STOP',
+                '*.pvd', '*.vtp', 'VTU_FRAME_INDEX.TXT']
+        outputs = []
+        for pat in patterns:
+            outputs.extend(glob.glob(os.path.join(project_dir, pat)))
+        return outputs
