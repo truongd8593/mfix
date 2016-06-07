738fb5880d9d6b0df4304c54ddd031236b617304
Charles G Waldman <cgw@alum.mit.edu>
2016-06-07 15:12:17 -0500

    fix comment

--- gui/constants.py
+++ gui/constants.py
@@ -1,7 +1,7 @@
 # Constants
 
 # Solver types
-# must match combobox_solver in model_setup.ui
+# must match combobox_solver in model.ui
 SINGLE, TFM, DEM, PIC, HYBRID = range(5)
 CONSTANT, AIR, UDF = 0, 1, 2 # model types
 MIXTURE=OTHER=AIR  # continuum, etc
