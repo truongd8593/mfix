510136d2f6bb1972a067bbbd129398bdffba2610
Mark Meredith <mwm126@gmail.com>
2016-06-17 14:02:17 -0400

    revert init2

--- model/main.f
+++ model/main.f
@@ -58,8 +58,14 @@
       USE output_man, only: init_output_vars, output_manager
       USE parallel_mpi, only: parallel_init, parallel_fin
       USE param1, only: n_spx, undefined, zero
-      USE run, only: id_version
-      USE run, only: time
+      USE pgcor, only: d_e, d_n, d_t, phase_4_p_g, switch_4_p_g
+      USE physprop, only: mmax
+      USE pscor, only: e_e, e_n, e_t, do_p_s, phase_4_p_s, mcp, switch_4_p_s
+      USE qmom_kinetic_equation, only: qmomk
+      USE read_input, only: get_data
+      USE run, only: id_version, ier
+      ! USE run, only: automatic_restart, call_usr, dem_solids, dt_max, dt_min
+      USE run, only: iter_restart, nstep, pic_solids, run_type, dt, shear, time, v_sh
       USE time_cpu, only: CPU00, wall0
 
       IMPLICIT NONE
@@ -119,57 +125,10 @@
 ! queue terminates. timestep at the beginning of execution
       CALL CPU_TIME (CPU00)
       WALL0 = WALL_TIME()
-   END SUBROUTINE INITIALIZE
 
-
-      SUBROUTINE INITIALIZE_2
-#ifdef MPI
-      USE mpi, only: mpi_comm_world, mpi_barrier
-#endif
-      USE cdist, only: bglobalnetcdf, bstart_with_one_res, bdist_io, bwrite_netcdf
-      USE check, only: check_mass_balance
-      USE check_data_cg, only: check_bc_flags, report_best_processor_size
-      USE coeff, only: init_coeff
-      USE compar, only: mpierr, mype, pe_io
-      USE cont, only: do_cont
-      USE cutcell, only: cartesian_grid, re_indexing, set_corner_cells
-      USE dbg, only: debug_write_layout, write_parallel_info
-      USE discretelement, only: discrete_element
-      USE drag, only: f_gs
-      USE error_manager, only: err_msg, flush_err_msg
-      USE error_manager, only: init_err_msg, finl_err_msg
-      USE fldvar, only: rop_g, rop_s
-      USE funits, only: dmp_log, unit_log, unit_res
-      USE machine, only: start_log, end_log
-      USE machine, only: wall_time, pc_quickwin, machine_cons, get_run_id, start_log, end_log
-      USE mfix_netcdf, only: mfix_usingnetcdf
-      USE output, only: dbgprn_layout
-      USE output_man, only: init_output_vars, output_manager
-      USE parallel_mpi, only: parallel_init, parallel_fin
-      USE param1, only: n_spx, undefined, zero
-      USE pgcor, only: d_e, d_n, d_t, phase_4_p_g, switch_4_p_g
-      USE physprop, only: mmax
-      USE pscor, only: e_e, e_n, e_t, do_p_s, phase_4_p_s, mcp, switch_4_p_s
-      USE qmom_kinetic_equation, only: qmomk
-      USE run, only: call_usr, dem_solids, dt_max, dt_min
-      USE run, only: ier
-      USE run, only: nstep, pic_solids, run_type, dt, shear, time, v_sh
-      USE time_cpu, only: cpu_io, cpu_nlog, cpu0, cpuos, time_nlog
-      USE vtk, only: write_vtk_files
-      IMPLICIT NONE
-
-      !$    INTEGER threads_specified
-
-      ! Temporary storage for DT
-      DOUBLE PRECISION :: DT_tmp
-      ! Save TIME in input file for RESTART_2
-      DOUBLE PRECISION :: TIME_SAVE
-
-      INTEGER :: LL, MM
-
-! Check data, do computations for IC and BC locations
+! Read input data, check data, do computations for IC and BC locations
 ! and flows, and set geometry parameters such as X, X_E, DToDX, etc.
-      CALL CHECK_DATA
+      CALL GET_DATA
 
 ! Write the initial part of the standard output file
       CALL WRITE_OUT0
@@ -446,151 +405,7 @@
       CALL MARK_PHASE_4_COR (PHASE_4_P_G, PHASE_4_P_S, DO_CONT, MCP,&
            DO_P_S, SWITCH_4_P_G, SWITCH_4_P_S)
 
-      END SUBROUTINE INITIALIZE_2
-
-      SUBROUTINE CHECK_DATA
-         USE check_data_cg, only: adjust_ijk_size, check_data_cartesian
-         USE constant, only: set_constants
-         USE cut_cell_preproc, only: cut_cell_preprocessing
-         USE cutcell, ONLY: cartesian_grid
-         USE dashboard, ONLY: dtmax, dtmin, init_time, n_dashboard, nit_max, nit_min, smmax, smmin
-         USE des_allocate
-         USE desgrid, only: DESGRID_INIT
-         USE discretelement, ONLY: discrete_element
-         USE error_manager
-         USE gridmap
-         USE iterate, ONLY: max_nit
-         USE mpi_init_des, only: DESMPI_INIT
-         USE param1, ONLY: large_number, n_spx
-         USE qmom_kinetic_equation, ONLY: qmomk
-         USE run, ONLY: run_name, run_type, time
-         USE stl_preproc_des, only: DES_STL_PREPROCESSING
-         IMPLICIT NONE
-
-         !-----------------------------------------------
-         ! Local variables
-         !-----------------------------------------------
-         ! shift DX, DY and DZ values
-         LOGICAL, PARAMETER :: SHIFT = .TRUE.
-
-! Initialize the error manager. This call occurs after the mfix.dat
-! is read so that message verbosity can be set and the .LOG file
-! can be opened.
-      CALL INIT_ERROR_MANAGER
-
-! Write header in the .LOG file and to screen.
-! Not sure if the values are correct or useful
-      CALL WRITE_HEADER
-
-! Open files
-      CALL OPEN_FILES(RUN_NAME, RUN_TYPE, N_SPX)
-
-! These checks verify that sufficient information was provided
-! to setup the domain indices and DMP gridmap.
-      CALL CHECK_GEOMETRY_PREREQS
-      CALL CHECK_DMP_PREREQS
-
-! Set up the physical domain indicies (cell index max/min values).
-      CALL SET_MAX2
-
-! Set constants
-      CALL SET_CONSTANTS
-
-! Adjust partition for better load balance (done when RE_INDEXING is .TRUE.)
-      CALL ADJUST_IJK_SIZE
-
-! Partition the domain and set indices
-      CALL GRIDMAP_INIT
-
-! Check the minimum solids phase requirements.
-      CALL CHECK_SOLIDS_MODEL_PREREQS
-
-      CALL CHECK_RUN_CONTROL
-      CALL CHECK_NUMERICS
-      CALL CHECK_OUTPUT_CONTROL
-
-      CALL CHECK_GAS_PHASE
-      CALL CHECK_SOLIDS_PHASES
-      CALL SET_PARAMETERS
-
-! Basic geometry checks.
-      CALL CHECK_GEOMETRY(SHIFT)
-      IF(DISCRETE_ELEMENT) CALL CHECK_GEOMETRY_DES
-
-! Set grid spacing variables.
-      CALL SET_GEOMETRY
-      IF(DISCRETE_ELEMENT) CALL SET_GEOMETRY_DES
-
-      CALL CHECK_INITIAL_CONDITIONS
-      CALL CHECK_BOUNDARY_CONDITIONS
-      CALL CHECK_INTERNAL_SURFACES
-      CALL CHECK_POINT_SOURCES
-
-      CALL CHECK_CHEMICAL_RXNS
-      CALL CHECK_ODEPACK_STIFF_CHEM
-
-
-
-!----------------------  DOMAIN SPECIFIC CHECKS  --------------------!
-
-
-! This call needs to occur before any of the IC/BC checks.
-      CALL SET_ICBC_FLAG
-
-! Compute area of boundary surfaces.
-      CALL GET_BC_AREA
-
-! Convert (mass, volume) flows to velocities.
-      CALL SET_BC_FLOW
-
-! Set the flags for identifying computational cells
-      CALL SET_FLAGS
-! Set arrays for computing indices
-      CALL SET_INCREMENTS
-      CALL SET_INCREMENTS3
-
-! Cartesian grid implementation
-      CALL CHECK_DATA_CARTESIAN
-      IF(CARTESIAN_GRID) THEN
-         CALL CUT_CELL_PREPROCESSING
-      ELSE
-         CALL ALLOCATE_DUMMY_CUT_CELL_ARRAYS
-      ENDIF
-
-      IF(DISCRETE_ELEMENT) THEN
-         CALL DESGRID_INIT
-         CALL DESMPI_INIT
-         CALL DES_STL_PREPROCESSING
-      ENDIF
-
-!--------------------------  ARRAY ALLOCATION -----------------------!
-
-! Allocate array storage.
-      CALL ALLOCATE_ARRAYS
-      IF(DISCRETE_ELEMENT) CALL DES_ALLOCATE_ARRAYS
-      IF(QMOMK) CALL QMOMK_ALLOCATE_ARRAYS
-
-! Initialize arrays.
-      CALL INIT_FVARS
-      IF(DISCRETE_ELEMENT) CALL DES_INIT_ARRAYS
-
-!======================================================================
-! Data initialization for Dashboard
-!======================================================================
-      INIT_TIME = TIME
-      SMMIN =  LARGE_NUMBER
-      SMMAX = -LARGE_NUMBER
-
-      DTMIN =  LARGE_NUMBER
-      DTMAX = -LARGE_NUMBER
-
-      NIT_MIN = MAX_NIT
-      NIT_MAX = 0
-
-      N_DASHBOARD = 0
-
-
-      END SUBROUTINE CHECK_DATA
+      END SUBROUTINE INITIALIZE
 
       SUBROUTINE FINALIZE
 
