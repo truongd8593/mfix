1ad630a5ef2371600034c337075561ea7ae8dc2d
Charles G Waldman <cgw@alum.mit.edu>
2016-06-30 20:07:42 -0500

    new file

---
+++ gui/solids_dem.py
@@ -0,0 +1,13 @@
+# Methods to deal with solids tfm tab, slip off from solids_handler.py
+from __future__ import print_function, absolute_import, unicode_literals, division
+import logging
+log = logging.getLogger(__name__)
+
+from tools.general import get_combobox_item, set_item_enabled
+
+class SolidsDEM(object):
+    def init_solids_dem(self):
+        pass
+
+    def setup_dem_tab(self):
+        pass
