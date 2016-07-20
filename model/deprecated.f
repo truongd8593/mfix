3b72bb7941c790a0f7685318f0b37c5e1173950c
Charles G Waldman <cgw@alum.mit.edu>
2016-07-20 09:05:57 -0500

    UNKNOWN_KEYWORD:  fix typo 'mensioned' -> 'dimensioned'

--- model/deprecated.f
+++ model/deprecated.f
@@ -202,7 +202,7 @@
          'Unable to process line ',A,' of the mfix.dat file.',2/3x,    &
          A,2/1x,'Possible causes are',/3x,'* Incorrect or illegal ',   &
          'keyword format',/3x,'* Unknown or mistyped name',/3x,'* ',   &
-         'The mensioned item is too small (array overflow).', 2/1x,    &
+         'The dimensioned item is too small (array overflow).',2/1x,   &
          'Please see the user documentation and update the mfix.dat ', &
          'file. ',/1X,70('*')//)
 
