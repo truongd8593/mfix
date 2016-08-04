f3e63907b5f99f8ed62e0c443b0d63d5cf40ef78
Charles G Waldman <charlesgwaldman@gmail.com>
2016-08-04 21:59:16 +0000

    Revert "read in utf-8 for both Python 2 and 3"
    This reverts commit 59984ae6d0e9e4f4860e0f090fb099da73f8c7e8

--- gui/tools/keyword_args.py
+++ gui/tools/keyword_args.py
@@ -4,16 +4,15 @@
 # from mfix_user_guide
 
 from __future__ import print_function, absolute_import, unicode_literals, division
-import io
 import os
 
 thisdir = os.path.abspath(os.path.dirname(__file__))
 
 keyword_args = {}
-with io.open(os.path.join(thisdir, 'keyword_args.txt'), encoding='utf-8') as f:
+with open(os.path.join(thisdir, 'keyword_args.txt')) as f:
     for line in f:
         line = line.lower().strip()
-        key, rest = line.split('(')[:2]
+        key, rest = line.split('(')
         args = [arg.strip() for arg in rest[:-1].split(',')]
         keyword_args[key] = args
 
