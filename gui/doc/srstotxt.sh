9ceedfda96f923ca634e9d18a77c617b9a97ab3f
Charles G Waldman <cgw@alum.mit.edu>
2016-08-11 15:49:23 -0500

    change output filename

--- gui/doc/srstotxt.sh
+++ gui/doc/srstotxt.sh
@@ -4,8 +4,8 @@
 
 pdftotext -nopgbrk 'MFIX-UI SRS.pdf'
 
-# Clean out page headers, footers, non-ascii chars
+# Clean out page headers, footers, non-ascii chars, duplicate blank lines
 
 sed "s/ /#### /;s/ /## /;s/^o /# /;s/‘/'/g;s/’/'/g" < 'MFIX-UI SRS.txt' |
     egrep -v '^Software Requirements Specification|^UI$|^Page||' |
-    uniq > mfix_ui_srs.txt
+    uniq > spec.txt
