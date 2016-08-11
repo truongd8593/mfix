03ce9da14cddf1726eb8eb50e8928b4f4439530c
Charles G Waldman <cgw@alum.mit.edu>
2016-08-11 17:09:46 -0500

    formatting

--- gui/doc/srstotxt.sh
+++ gui/doc/srstotxt.sh
@@ -6,6 +6,6 @@ pdftotext -nopgbrk 'MFIX-UI SRS.pdf'
 
 # Clean out page headers, footers, non-ascii chars, duplicate blank lines
 
-sed "s/ /#### /;s/ /## /;s/^o /# /;s/‘/'/g;s/’/'/g" < 'MFIX-UI SRS.txt' |
+sed "s/ /#    /;s/ /#  /;s/^o /# /;s/‘/'/g;s/’/'/g" < 'MFIX-UI SRS.txt' |
     egrep -v '^Software Requirements Specification|^UI$|^Page||' |
     uniq > spec.txt
