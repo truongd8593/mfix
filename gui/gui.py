01e8a7eade71e3a9802b1c2f7f50ccb1da8a60e1
Charles G Waldman <cgw@alum.mit.edu>
2016-08-12 10:38:35 -0500

    remove incorrect warning

--- gui/gui.py
+++ gui/gui.py
@@ -2209,9 +2209,10 @@ class MfixGui(QtWidgets.QMainWindow,
                     pass # handled in 'geometry' section above
                 elif key == 'parameters':
                     pass # Where is this handled?
-                else:
-                    self.warn("Unknown mfix-gui setting '%s'" % key)
                 # Add more here
+                #else:  # Too many warnings!
+                #    self.warn("Unknown mfix-gui setting '%s'" % key)
+
         except Exception as e:
             self.error("%s: %s" % (key, e))
 
