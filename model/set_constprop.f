truongd8593/mfix

commit 4180e3fcacdddf16140669a09d920a2ece198946

Fix restart issue with variable solids density.

model/set_constprop.f
@@ -39,6 +39,7 @@
       USE constant, only: ep_s_max_ratio, d_p_ratio, ep_s_max, m_max
       use constant, only: ep_star
 
+      USE run, only: RUN_TYPE
       USE run, only: call_dqmom
       USE run, only: yu_standish, fedors_landel
       USE run, only: kt_type_enum, ia_2005, gd_1999, gtsh_2012
@@ -83,7 +84,10 @@
       DIF_g = ZERO
       F_GS = ZERO
 
-      RO_S = ZERO
+      IF (RUN_TYPE == 'NEW') RO_S = ZERO ! For variable density, RO_S is set
+                                         ! to the baseline density for new runs
+                                         ! further down in this file
+                                         ! and is read from .RES when restarting
       D_P = zero     ! this is done in init_fvars
       MU_s = ZERO
       EPMU_s = ZERO
@@ -182,7 +186,7 @@
          DO IJK = ijkstart3, ijkend3
             IF(.NOT.WALL_AT(IJK)) THEN
 ! Fluid and inflow/outflow cells: FLAG < 100
-               IF (RO_S0(M) /= UNDEFINED) RO_S(IJK,M) = RO_S0(M)
+               IF (RO_S0(M) /= UNDEFINED.AND.RUN_TYPE == 'NEW') RO_S(IJK,M) = RO_S0(M)
                IF (C_PS0(M) /= UNDEFINED) C_PS(IJK,M) = C_PS0(M)
                IF (D_P0(M) /= UNDEFINED) D_P(IJK,M) = D_P0(M)
             ENDIF

model/write_res1.f
@@ -70,6 +70,7 @@
           call send_recv(V_g,2)
           call send_recv(W_g,2)
           call send_recv(ROP_S,2)
+!          IF(ANY(SOLVE_ROs)) call send_recv(RO_S,2)
           call send_recv(T_S,2)
           call send_recv(U_S,2)
           call send_recv(V_S,2)

model/read_res1.f
@@ -192,6 +192,7 @@
          call send_recv(rop_g)
          call send_recv(ro_g)
          call send_recv(rop_s)
+         call send_recv(ro_s) 
       end if
 
 

commit 3d407e1cc177a566ac64949bc2406f6260cabad0

fix bug with restart introduced in commit 4180e3fcacdddf16140669a09d920a2ece198946
 -the previous commit introduced a bug in restarts wherein ro_s
  was no longer being set for constant solids density cases.

model/set_constprop.f
@@ -40,7 +40,7 @@
       use constant, only: ep_star
 
       USE run, only: RUN_TYPE
-      USE run, only: call_dqmom
+      USE run, only: call_dqmom, solve_ros
       USE run, only: yu_standish, fedors_landel
       USE run, only: kt_type_enum, ia_2005, gd_1999, gtsh_2012
       USE run, only: blending_stress, sigm_blend, tanh_blend
@@ -84,10 +84,10 @@
       DIF_g = ZERO
       F_GS = ZERO
 
-      IF (RUN_TYPE == 'NEW') RO_S = ZERO ! For variable density, RO_S is set
-                                         ! to the baseline density for new runs
-                                         ! further down in this file
-                                         ! and is read from .RES when restarting
+! For variable density: RO_S will be set to the baseline density for
+! new runs but was already read-in from read_res1 for restart runs.
+      IF (RUN_TYPE == 'NEW') RO_S = ZERO 
+
       D_P = zero     ! this is done in init_fvars
       MU_s = ZERO
       EPMU_s = ZERO
@@ -186,7 +186,7 @@
          DO IJK = ijkstart3, ijkend3
             IF(.NOT.WALL_AT(IJK)) THEN
 ! Fluid and inflow/outflow cells: FLAG < 100
-               IF (RO_S0(M) /= UNDEFINED.AND.RUN_TYPE == 'NEW') RO_S(IJK,M) = RO_S0(M)
+               IF (RO_S0(M) /= UNDEFINED.AND..NOT.SOLVE_ROs(M)) RO_S(IJK,M) = RO_S0(M)
                IF (C_PS0(M) /= UNDEFINED) C_PS(IJK,M) = C_PS0(M)
                IF (D_P0(M) /= UNDEFINED) D_P(IJK,M) = D_P0(M)
             ENDIF

model/write_res1.f
@@ -69,7 +69,7 @@
           call send_recv(V_g,2)
           call send_recv(W_g,2)
           call send_recv(ROP_S,2)
-!          IF(ANY(SOLVE_ROs)) call send_recv(RO_S,2)
+          IF(ANY(SOLVE_ROs)) call send_recv(RO_S,2)
           call send_recv(T_S,2)
           call send_recv(U_S,2)
           call send_recv(V_S,2)