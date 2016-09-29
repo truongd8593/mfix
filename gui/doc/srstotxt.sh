257bdad0c04e1d6331dc62a56be022f37b4a0483
Charles G Waldman <cgw@alum.mit.edu>
2016-09-29 15:59:59 -0500

    updates to SRS, remove item numbering

--- gui/doc/srstotxt.sh
+++ gui/doc/srstotxt.sh
@@ -7,6 +7,5 @@ pdftotext -nopgbrk 'MFIX-UI SRS.pdf'
 # Clean out page headers, footers, non-ascii chars, duplicate blank lines
 
 sed "s/ /#    /;s/ /#  /;s/^o /# /;s/‘/'/g;s/’/'/g" < 'MFIX-UI SRS.txt' |
-    sed 's/^[0-9]*\. /# /' |
     egrep -v '^Software Requirements Specification|^UI$|^Page||' |
     uniq > spec.txt
