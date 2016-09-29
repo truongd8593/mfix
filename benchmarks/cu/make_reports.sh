4265f666318e7dd30721d917e80404a265059d92
Mark Meredith <Mark.Meredith@contr.netl.doe.gov>
2016-09-29 14:01:27 -0400

    generate reports better

--- benchmarks/cu/make_reports.sh
+++ benchmarks/cu/make_reports.sh
@@ -1,12 +1,15 @@
-#!/bin/bash -le
+#!/bin/bash -l
 
 module load intel
-DIRS=~/exa_profiling/DEM0[1-4]/[1-4]/*
+DIRS=~/exa_profiling/DEM0[1-4]/[1-4]
 
 for DIR in ${DIRS}; do
-    echo  > ${DIR}.report
-    echo >> ${DIR}.report
-    echo " DATA FOR DIRECTORY: ${DIR}" >> ${DIR}.report
-    amplxe-cl -report summary -r ${DIR} >>  ${DIR}.report
-    amplxe-cl -report hotspots -r ${DIR} | head -n 10 | cut -c 1-80 >> ${DIR}.report
+    RESULTS=$( find ${DIR}/* -maxdepth 0 -type d )
+    for RESULT in ${RESULTS}; do
+        echo  > ${RESULT}.report
+        echo >> ${RESULT}.report
+        echo " DATA FOR DIRECTORY: ${RESULT}" >> ${RESULT}.report
+        amplxe-cl -report summary -r ${RESULT} >>  ${RESULT}.report
+        amplxe-cl -report hotspots -r ${RESULT} | head -n 10 | cut -c 1-80 >> ${RESULT}.report
+    done
 done
