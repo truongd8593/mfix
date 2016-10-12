62e13e48c6263b377bada300269fa22655219853
Charles G Waldman <cgw@alum.mit.edu>
2016-10-12 18:35:17 -0500

    work on Chemistry, reaction names, species by phase

--- gui/species_handler.py
+++ gui/species_handler.py
@@ -4,11 +4,11 @@ from __future__ import print_function, absolute_import, unicode_literals, divisi
 
 class SpeciesHandler(object):
     def species_all_aliases(self):
-        for k in self.fluid_species.keys():
-            yield k
+        for (name, data) in self.fluid_species.items():
+            data.get('alias', name)
         for phase in self.solids_species.values():
-            for k in phase.keys():
-                yield k
+            for (name, data) in phase.keys():
+                yield data.get('alias', name)
 
     def species_alias_unique(self, alias):
         return alias not in self.species_all_aliases()
@@ -29,3 +29,28 @@ class SpeciesHandler(object):
             if self.species_alias_unique(alias):
                 return alias
             count += 1
+
+
+    def find_species_phase(self, species_or_alias):
+        if not self.fluid_solver_disabled:
+            if species_or_alias in self.fluid_species:
+                return 0
+            for (name, data) in self.fluid_species.items():
+                if data.get('alias') == species_or_alias:
+                    return 0
+
+        for (p, s) in self.solids_species.items():
+            if species_or_alias in s:
+                return p
+            for (name, data) in s.items():
+                if data.get('alias') == species_or_alias:
+                    return p
+
+
+    def species_of_phase(self, p):
+        if p == 0:
+            return [data.get('alias', name)
+                    for (name, data) in self.fluid_species.items()]
+        else:
+            return [data.get('alias', name)
+                    for (name, data) in self.solids_species[p].items()]
