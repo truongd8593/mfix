ec905cbacc7a267597ab67483aa030f7d3e54911
Janine Carney <janine.carney@netl.doe.gov>
2016-10-21 17:39:49 -0400

    move ro_s = ro_s0 line back under if species
    -unclear the full reason for the failures when this line is not
     under if(species). this will need to be pursued at some later
     point

--- model/reset_new.f
+++ model/reset_new.f
@@ -72,10 +72,10 @@
           IF (NMAX(M) > 0) THEN
             X_S(:,M,:NMAX(M)) = X_SO(:,M,:NMAX(M))
           ENDIF
-        ENDIF
 ! species_eq do not have to be solved to involve varying density
 ! (could be user defined function)
-        RO_S(:,M) = RO_SO(:,M)
+          RO_S(:,M) = RO_SO(:,M)
+        ENDIF
       ENDDO
 
 ! Recalculate all coefficients
