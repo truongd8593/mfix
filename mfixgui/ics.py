c2c8278ef777664f98564b687b0e7ab29f02dc1b
Charles G Waldman <cgw@alum.mit.edu>
2017-01-20 12:49:41 -0600

    ICs:  table size fixups

--- mfixgui/ics.py
+++ mfixgui/ics.py
@@ -348,6 +348,10 @@ class ICS(object):
             tw.setMaximumHeight(height)
             tw.setMinimumHeight(header_height)
         else: # mass fraction tables
+            if tw == ui.tablewidget_solids_mass_fraction:
+                ui.groupbox_solids_composition.setMaximumHeight(height+30)
+            elif tw == ui.tablewidget_fluid_mass_fraction:
+                ui.groupbox_fluid_composition.setMaximumHeight(height+30)
             tw.setMaximumHeight(height) # Works for tablewidget inside groupbox
             tw.setMinimumHeight(height) #? needed? should we allow scrollbar?
         tw.updateGeometry() #? needed?
@@ -697,7 +701,6 @@ class ICS(object):
             self.update_ics_fluid_mass_fraction_table()
 
 
-    # DRY out fluid/solids code
     def update_ics_solids_mass_fraction_table(self):
         ui = self.ui.initial_conditions
         table = ui.tablewidget_solids_mass_fraction
@@ -746,7 +749,6 @@ class ICS(object):
             font.setBold(True)
             item.setFont(font)
             self.update_ics_solids_mass_fraction_total()
-
         self.fixup_ics_table(table)
 
 
