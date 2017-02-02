4c17a446e01ed57f8e023cab723b0b3cd1f9ea3e
Mark Meredith <mwm126@gmail.com>
2017-02-02 12:46:20 -0500

    numpy 1.11 means 1.11.0, not latest 11.11.3

--- setup.py
+++ setup.py
@@ -141,7 +141,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'flask',
-        'numpy==1.11',
+        'numpy==1.11.3',
         'packaging',
         'qtpy',
     ],
