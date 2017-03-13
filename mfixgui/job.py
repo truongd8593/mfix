3edaac5068468929ae3b12ad1420f0ce09c82330
Charles G Waldman <cgw@alum.mit.edu>
2017-03-13 13:07:01 -0500

    standard imports

--- mfixgui/job.py
+++ mfixgui/job.py
@@ -1,12 +1,6 @@
-"""
-job.py
-======
-
-This module defines classes used to facilitate MFiX job management.
-
-:platform: Unix, Windows
-:license: Public Domain
-"""
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
 import json
 import logging
