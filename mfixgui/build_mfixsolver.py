528518ef467f72841818828e2d92102a3214ba6d
Mark Meredith <Mark.Meredith@netl.doe.gov>
2017-03-14 17:37:22 -0400

    another fix

--- mfixgui/build_mfixsolver.py
+++ mfixgui/build_mfixsolver.py
@@ -43,11 +43,12 @@ def make_mfixsolver():
     build_dir = os.path.join('build', configure_args_dirname)
 
     udfs = []
-    for root, dirnames, filenames in os.walk('.'):
-        for filename in glob.glob(os.path.join(root, '*.f')):
-            udfs.append(filename)
+    if not os.path.isfile('configure_mfix'):
+        for root, dirnames, filenames in os.walk('.'):
+            for filename in glob.glob(os.path.join(root, '*.f')):
+                udfs.append(filename)
 
-    udfs = [ os.path.splitext(f)[0]+'.o' for f in udfs if not f.startswith('build') ]
+        udfs = [ os.path.splitext(f)[0]+'.o' for f in udfs if not f.startswith('build') ]
 
     extra_objects = udfs + [
         '.build/read_database.o',
